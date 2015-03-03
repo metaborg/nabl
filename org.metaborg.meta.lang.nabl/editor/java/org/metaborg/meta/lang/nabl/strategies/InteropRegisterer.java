96ee1606d2145bff5b160d5def8a77edaf8ada65
Gabriel Konat <gabrielkonat@gmail.com>
2015-03-03 16:52:17 +0100

    Integrate with new Spoofax Eclipse plugins.

--- org.metaborg.meta.lang.nabl/editor/java/org/metaborg/meta/lang/nabl/strategies/InteropRegisterer.java
+++ org.metaborg.meta.lang.nabl/editor/java/org/metaborg/meta/lang/nabl/strategies/InteropRegisterer.java
@@ -4,7 +4,7 @@ import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
-	public InteropRegisterer() {
-		super(new Strategy[] {});
-	}
+    public InteropRegisterer() {
+        super(new Strategy[] {});
+    }
 }
