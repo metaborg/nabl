991ce3e9711aaa68288d1b2a3acc27bbabd5c405
Moritz Lichter <lichtemo@googlemail.com>
2015-10-26 18:13:23 +0100

    Adapt InteropRegisterer

--- org.metaborg.meta.lang.nabl/editor/java/org/metaborg/meta/lang/nabl/strategies/InteropRegisterer.java
+++ org.metaborg.meta.lang.nabl/editor/java/org/metaborg/meta/lang/nabl/strategies/InteropRegisterer.java
@@ -1,10 +1,9 @@
 package org.metaborg.meta.lang.nabl.strategies;
 
 import org.strategoxt.lang.JavaInteropRegisterer;
-import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
 	public InteropRegisterer() {
-		super(new Strategy[] {});
+		super(new LibraryInitializer());
 	}
 }
