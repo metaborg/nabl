6ec9cdaa86b4eca5314685cd6f075090ccc2292b
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2016-12-21 20:29:21 +0100

    Work on dynsem code generation.

--- org.metaborg.meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/spoofax/InitialResult.java
+++ org.metaborg.meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/spoofax/InitialResult.java
@@ -10,7 +10,6 @@ import org.metaborg.meta.nabl2.solver.SolverConfig;
 import org.metaborg.meta.nabl2.terms.ITerm;
 import org.metaborg.meta.nabl2.terms.Terms.IMatcher;
 import org.metaborg.meta.nabl2.terms.Terms.M;
-import org.metaborg.util.iterators.Iterables2;
 
 @Value.Immutable
 @Serial.Version(value = 42L)
@@ -25,28 +24,10 @@ public abstract class InitialResult {
     public abstract Optional<ITerm> getCustomResult();
 
     public static IMatcher<ImmutableInitialResult> matcher() {
-        return M.appl3("InitialResult", ConstraintTerms.constraints(), args(), SolverConfig.matcher(), (t, constraints,
+        return M.appl3("InitialResult", ConstraintTerms.constraints(), Args.matcher(), SolverConfig.matcher(), (t, constraints,
                 args, config) -> {
             return ImmutableInitialResult.of(constraints, args, config);
         });
-    }
-
-    private static IMatcher<Args> args() {
-        return M.cases(
-            // @formatter:off
-            M.appl1("Params", params(), (a,ps) -> ImmutableArgs.of(ps, Optional.empty())),
-            M.appl2("ParamsAndType", params(), M.term(), (a, ps, t) -> ImmutableArgs.of(ps, Optional.of(t)))
-            // @formatter:on
-        );
-    }
-
-    private static IMatcher<Iterable<ITerm>> params() {
-        return M.cases(
-            // @formatter:off
-            M.appl("", (a) -> a.getArgs()),
-            M.term(p -> Iterables2.singleton(p))
-            // @formatter:on
-        );
     }
 
 }
