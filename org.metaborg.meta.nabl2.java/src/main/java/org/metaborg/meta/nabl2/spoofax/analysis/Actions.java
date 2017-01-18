03b8852b868f4339a50a8f5dab9c6419ad322e56
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2017-01-18 16:48:36 +0100

    Give origin to initial term, getting rid of warning during analysis.

--- org.metaborg.meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/spoofax/analysis/Actions.java
+++ org.metaborg.meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/spoofax/analysis/Actions.java
@@ -4,6 +4,7 @@ import org.metaborg.meta.nabl2.terms.ITerm;
 import org.metaborg.meta.nabl2.terms.generic.GenericTerms;
 import org.metaborg.meta.nabl2.terms.generic.ImmutableTermIndex;
 import org.metaborg.meta.nabl2.terms.generic.TermIndex;
+import org.spoofax.jsglr.client.imploder.ImploderAttachment;
 
 import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.collect.Iterables;
@@ -22,7 +23,6 @@ public class Actions {
         return GenericTerms.newAppl("AnalyzeFinal", sourceTerm(resource));
     }
 
-
     public static ITerm customInitial(String resource) {
         return GenericTerms.newAppl("CustomInitial", sourceTerm(resource));
     }
@@ -35,16 +35,17 @@ public class Actions {
         return GenericTerms.newAppl("CustomFinal", sourceTerm(resource), initial, GenericTerms.newList(units));
     }
 
-
     private static ITerm sourceTerm(String resource) {
-        return GenericTerms.newString(resource, ImmutableClassToInstanceMap.builder().put(TermIndex.class,
-                ImmutableTermIndex.of(resource, 0)).build());
+        ImploderAttachment origin = ImploderAttachment.createCompactPositionAttachment(resource, 0, 0, 0, 0);
+        ImmutableTermIndex index = ImmutableTermIndex.of(resource, 0);
+        return GenericTerms.newString(resource, ImmutableClassToInstanceMap.builder().put(TermIndex.class, index)
+                .put(ImploderAttachment.class, origin).build());
     }
 
     private static ITerm args(Args args) {
         Iterable<ITerm> paramTerms = args.getParams();
         ITerm paramsTerm;
-        if (Iterables.size(paramTerms) == 1) {
+        if(Iterables.size(paramTerms) == 1) {
             paramsTerm = Iterables.getOnlyElement(paramTerms);
         } else {
             paramsTerm = GenericTerms.newTuple(paramTerms);
