c6a3df28559e102a5c4a2b540deacc54d2181869
Michael Steindorfer <michael@steindorfer.name>
2017-05-09 15:26:44 +0200

    Formatting with spaces.

--- meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/scopegraph/esop/IEsopScopeGraph.java
+++ meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/scopegraph/esop/IEsopScopeGraph.java
@@ -17,57 +17,57 @@ import io.usethesource.capsule.Set;
 
 @Beta
 public interface IEsopScopeGraph<S extends IScope, L extends ILabel, O extends IOccurrence>
-		extends IScopeGraph<S, L, O> {
+        extends IScopeGraph<S, L, O> {
 
     public static final boolean USE_PERSISTENT_SCOPE_GRAPH = Boolean.getBoolean("usePersistentScopeGraph");
-  
-	/*
-	 * Factory method to switch between different scope graph implementations.
-	 */
-	static <S extends IScope, L extends ILabel, O extends IOccurrence> IEsopScopeGraph.Builder<S, L, O> builder() {    
-	    if (USE_PERSISTENT_SCOPE_GRAPH) {
-	        return new PersistentScopeGraph.Builder<>();
-	    } else {	    
-	        return new EsopScopeGraph<>();
-	    }
-	}
+
+    /*
+     * Factory method to switch between different scope graph implementations.
+     */
+    static <S extends IScope, L extends ILabel, O extends IOccurrence> IEsopScopeGraph.Builder<S, L, O> builder() {
+        if (USE_PERSISTENT_SCOPE_GRAPH) {
+            return new PersistentScopeGraph.Builder<>();
+        } else {
+            return new EsopScopeGraph<>();
+        }
+    }
 
     IEsopNameResolution<S, L, O> resolve(IResolutionParameters<L> params, OpenCounter<S, L> scopeCounter);
 
-	interface Builder<S extends IScope, L extends ILabel, O extends IOccurrence> {
+    interface Builder<S extends IScope, L extends ILabel, O extends IOccurrence> {
 
-		Set<S> getAllScopes();
+        Set<S> getAllScopes();
 
-		Set<O> getAllDecls();
+        Set<O> getAllDecls();
 
-		Set<O> getAllRefs();
+        Set<O> getAllRefs();
 
-		IFunction<O, S> getDecls();
+        IFunction<O, S> getDecls();
 
-		IFunction<O, S> getRefs();
+        IFunction<O, S> getRefs();
 
-		IRelation3<S, L, S> getDirectEdges();
+        IRelation3<S, L, S> getDirectEdges();
 
-		IRelation3<O, L, S> getExportEdges();
+        IRelation3<O, L, S> getExportEdges();
 
-		IRelation3<S, L, O> getImportEdges();
+        IRelation3<S, L, O> getImportEdges();
 
-		// -----------------------
+        // -----------------------
 
-		void addDecl(S scope, O decl);
+        void addDecl(S scope, O decl);
 
-		void addRef(O ref, S scope);
+        void addRef(O ref, S scope);
 
-		void addDirectEdge(S sourceScope, L label, S targetScope);
+        void addDirectEdge(S sourceScope, L label, S targetScope);
 
-		void addAssoc(O decl, L label, S scope);
+        void addAssoc(O decl, L label, S scope);
 
-		void addImport(S scope, L label, O ref);
+        void addImport(S scope, L label, O ref);
 
-		// -----------------------
+        // -----------------------
 
-		IEsopScopeGraph<S, L, O> result();
+        IEsopScopeGraph<S, L, O> result();
 
-	}
+    }
 
 }
