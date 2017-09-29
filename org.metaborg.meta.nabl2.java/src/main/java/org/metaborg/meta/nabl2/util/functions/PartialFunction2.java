830eb6a246cfabdd4686befc8d7390aa4cb42f2e
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2017-09-29 18:45:32 +0200

    Remove left-overs from merge.

--- org.metaborg.meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/util/functions/PartialFunction2.java
+++
@@ -1,8 +0,0 @@
-package org.metaborg.meta.nabl2.util.functions;
-
-import java.util.Optional;
-
-@FunctionalInterface
-public interface PartialFunction2<T1, T2, R> extends Function2<T1, T2, Optional<R>> {
-
-}
