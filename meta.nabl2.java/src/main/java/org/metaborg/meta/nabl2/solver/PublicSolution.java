bfbf9b43be3b6ebcde24b1bac3f7703112da5149
Jeff Smits <jeff.smits@gmail.com>
2017-10-03 17:14:09 +0200

    Fix small issues

--- meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/solver/PublicSolution.java
+++ meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/solver/PublicSolution.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.immutables.serial.Serial;
 import org.immutables.value.Value;
+import org.metaborg.meta.nabl2.controlflow.terms.CFGNode;
 import org.metaborg.meta.nabl2.relations.variants.IVariantRelation;
 import org.metaborg.meta.nabl2.relations.variants.VariantRelations;
 import org.metaborg.meta.nabl2.scopegraph.esop.IEsopNameResolution;
@@ -14,9 +15,13 @@ import org.metaborg.meta.nabl2.scopegraph.terms.Label;
 import org.metaborg.meta.nabl2.scopegraph.terms.Occurrence;
 import org.metaborg.meta.nabl2.scopegraph.terms.Scope;
 import org.metaborg.meta.nabl2.symbolic.ISymbolicConstraints;
+import org.metaborg.meta.nabl2.symbolic.SymbolicConstraints;
 import org.metaborg.meta.nabl2.terms.ITerm;
 import org.metaborg.meta.nabl2.util.collections.IProperties;
 import org.metaborg.meta.nabl2.util.collections.Properties;
+
+import meta.flowspec.nabl2.controlflow.IControlFlowGraph;
+import meta.flowspec.nabl2.controlflow.impl.ControlFlowGraph;
 
 @Value.Immutable(builder = true)
 @Serial.Version(value = 1L)
@@ -33,12 +38,15 @@ public abstract class PublicSolution implements IPublicSolution {
     @Value.Parameter @Override public abstract Map<String, IVariantRelation.Immutable<ITerm>> relations();
 
     @Value.Parameter @Override public abstract ISymbolicConstraints symbolic();
+    
+    @Value.Parameter @Override public abstract IControlFlowGraph<CFGNode> controlFlowGraph();
 
     public static IPublicSolution of(SolverConfig config) {
         return ImmutablePublicSolution.of(config, EsopScopeGraph.Immutable.of(),
                 EsopNameResolution.Immutable.of(config.getResolutionParams()), Properties.Immutable.of(),
                 VariantRelations.immutableOf(config.getRelations()),
-                org.metaborg.meta.nabl2.symbolic.SymbolicConstraints.of());
+                SymbolicConstraints.of(),
+                ControlFlowGraph.of());
     }
 
 }
