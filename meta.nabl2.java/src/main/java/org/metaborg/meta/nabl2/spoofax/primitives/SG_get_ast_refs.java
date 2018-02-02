d9c8b6d233137d9604d383601c489e7d18f854d0
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-02-02 17:28:30 +0100

    Use AnalysisPrimitive supertype.

--- meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/spoofax/primitives/SG_get_ast_refs.java
+++ meta.nabl2.java/src/main/java/org/metaborg/meta/nabl2/spoofax/primitives/SG_get_ast_refs.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.metaborg.meta.nabl2.scopegraph.terms.Occurrence;
-import org.metaborg.meta.nabl2.spoofax.analysis.IScopeGraphContext;
-import org.metaborg.meta.nabl2.stratego.ITermIndex;
+import org.metaborg.meta.nabl2.spoofax.analysis.IScopeGraphUnit;
 import org.metaborg.meta.nabl2.stratego.TermIndex;
 import org.metaborg.meta.nabl2.terms.ITerm;
 import org.metaborg.meta.nabl2.terms.generic.TB;
@@ -13,24 +12,19 @@ import org.spoofax.interpreter.core.InterpreterException;
 
 import com.google.common.collect.Lists;
 
-public class SG_get_ast_refs extends ScopeGraphContextPrimitive {
+public class SG_get_ast_refs extends AnalysisPrimitive {
 
     public SG_get_ast_refs() {
-        super(SG_get_ast_refs.class.getSimpleName(), 0, 1);
+        super(SG_get_ast_refs.class.getSimpleName());
     }
 
-    @Override public Optional<ITerm> call(IScopeGraphContext<?> context, ITerm term, List<ITerm> terms)
+    @Override public Optional<ITerm> call(IScopeGraphUnit unit, ITerm term, List<ITerm> terms)
             throws InterpreterException {
-        if (terms.size() != 1) {
-            throw new InterpreterException("Need one term argument: analysis");
-        }
-        final ITermIndex analysis = TermIndex.get(terms.get(0))
-                .orElseThrow(() -> new InterpreterException("Not an analysis term."));
         return TermIndex.get(term).flatMap(index -> {
-            return context.unit(analysis.getResource()).solution().<ITerm> flatMap(s -> {
+            return unit.solution().<ITerm>flatMap(s -> {
                 List<ITerm> entries = Lists.newArrayList();
-                for (Occurrence ref : s.scopeGraph().getAllRefs()) {
-                    if (ref.getIndex().equals(index)) {
+                for(Occurrence ref : s.scopeGraph().getAllRefs()) {
+                    if(ref.getIndex().equals(index)) {
                         entries.add(ref);
                     }
                 }
