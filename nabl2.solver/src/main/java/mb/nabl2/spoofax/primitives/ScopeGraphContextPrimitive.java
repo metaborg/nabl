1f252a5de2ca2a4753ef243814aa166dd7d4e728
Jeff Smits <jeff.smits@gmail.com>
2018-06-11 11:29:18 +0200

    Revert "Use fake scopegraph context when the context is not set up for NaBL2"
    This reverts commit 1e1bcaa44160e4fd5261ff4c2dba6ddd8cf66e1d.

--- nabl2.solver/src/main/java/mb/nabl2/spoofax/primitives/ScopeGraphContextPrimitive.java
+++ nabl2.solver/src/main/java/mb/nabl2/spoofax/primitives/ScopeGraphContextPrimitive.java
@@ -1,16 +1,11 @@
 package mb.nabl2.spoofax.primitives;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.metaborg.util.concurrent.IClosableLock;
-import org.metaborg.util.concurrent.NullClosableLock;
 import org.metaborg.util.log.ILogger;
 import org.metaborg.util.log.LoggerUtils;
 import org.spoofax.interpreter.core.IContext;
@@ -20,20 +15,12 @@ import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
 
-import mb.nabl2.config.NaBL2Config;
-import mb.nabl2.config.NaBL2DebugConfig;
-import mb.nabl2.constraints.IConstraint;
-import mb.nabl2.solver.Fresh;
-import mb.nabl2.solver.ISolution;
-import mb.nabl2.spoofax.analysis.CustomSolution;
 import mb.nabl2.spoofax.analysis.IScopeGraphContext;
-import mb.nabl2.spoofax.analysis.IScopeGraphUnit;
 import mb.nabl2.stratego.ConstraintTerms;
 import mb.nabl2.stratego.StrategoTerms;
 import mb.nabl2.terms.ITerm;
 
 public abstract class ScopeGraphContextPrimitive extends AbstractPrimitive {
-    private static final FakeScopeGraphContext FAKE_SCOPE_GRAPH_CONTEXT = new FakeScopeGraphContext();
     private static ILogger logger = LoggerUtils.logger(ScopeGraphContextPrimitive.class);
 
     public ScopeGraphContextPrimitive(String name, int svars, int tvars) {
@@ -47,12 +34,10 @@ public abstract class ScopeGraphContextPrimitive extends AbstractPrimitive {
             logger.warn("Context is null.");
             return false;
         }
-        final IScopeGraphContext<?> context;
-        if (!(contextObj instanceof IScopeGraphContext)) {
-            context = FAKE_SCOPE_GRAPH_CONTEXT;
-        } else {
-            context = (IScopeGraphContext<?>) env.contextObject();
+        if(!(contextObj instanceof IScopeGraphContext)) {
+            throw new InterpreterException("Context does not implement IScopeGraphContext");
         }
+        final IScopeGraphContext<?> context = (IScopeGraphContext<?>) env.contextObject();
         List<IStrategoTerm> termArgs = Arrays.asList(tvars);
         Optional<? extends IStrategoTerm> result;
         try (IClosableLock lock = context.guard()) {
@@ -79,67 +64,4 @@ public abstract class ScopeGraphContextPrimitive extends AbstractPrimitive {
             throws InterpreterException {
         throw new UnsupportedOperationException("Subclasses must override call method.");
     }
-
-    private static final class FakeScopeGraphContext implements IScopeGraphContext<IScopeGraphUnit> {
-        private static final NaBL2Config CONFIG = new NaBL2Config(false,
-                new NaBL2DebugConfig(NaBL2DebugConfig.Flag.ALL));
-
-        IScopeGraphUnit fakeUnit = new IScopeGraphUnit() {
-            private static final long serialVersionUID = 1L;
-            private Fresh fresh = new Fresh();
-
-            @Override
-            public String resource() {
-                return null;
-            }
-
-            @Override
-            public Set<IConstraint> constraints() {
-                return Collections.unmodifiableSet(new HashSet<>());
-            }
-
-            @Override
-            public Optional<ISolution> solution() {
-                return Optional.empty();
-            }
-
-            @Override
-            public Optional<CustomSolution> customSolution() {
-                return Optional.empty();
-            }
-
-            @Override
-            public Fresh fresh() {
-                return fresh;
-            }
-
-            @Override
-            public boolean isPrimary() {
-                return false;
-            }
-        };
-        Collection<IScopeGraphUnit> fakeUnits = Collections.unmodifiableList(Arrays.asList(fakeUnit));
-        IClosableLock l = new NullClosableLock();
-
-        @Override
-        public IClosableLock guard() {
-            return l;
-        }
-
-        @Override
-        public IScopeGraphUnit unit(String resource) {
-            return fakeUnit;
-        }
-
-        @Override
-        public Collection<IScopeGraphUnit> units() {
-            return fakeUnits;
-        }
-
-        @Override
-        public NaBL2Config config() {
-            return CONFIG;
-        }
-    }
-
 }
