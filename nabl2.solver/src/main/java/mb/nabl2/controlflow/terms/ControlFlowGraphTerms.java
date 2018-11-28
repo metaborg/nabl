e5cb6d9eaaa38c8d36b40c6c89c0fa492e3f86ba
Jeff Smits <jeff.smits@gmail.com>
2018-11-28 17:27:22 +0100

    Fold away entry/exit nodes in the graph so there are still edges left to show

--- nabl2.solver/src/main/java/mb/nabl2/controlflow/terms/ControlFlowGraphTerms.java
+++ nabl2.solver/src/main/java/mb/nabl2/controlflow/terms/ControlFlowGraphTerms.java
@@ -1,11 +1,12 @@
 package mb.nabl2.controlflow.terms;
 
-import io.usethesource.capsule.Set;
 import java.util.stream.Collectors;
 
 import org.metaborg.util.Ref;
 
+import io.usethesource.capsule.BinaryRelation;
 import io.usethesource.capsule.Map.Immutable;
+import io.usethesource.capsule.Set;
 import mb.nabl2.terms.ITerm;
 import mb.nabl2.util.ImmutableTuple2;
 import mb.nabl2.util.Tuple2;
@@ -28,17 +29,12 @@ public final class ControlFlowGraphTerms {
     private String toDot() {
         final java.util.Set<String> properties = this.preProperties.keySet().stream().map(t -> t._2()).collect(Collectors.toSet());
 
-        final Set.Immutable<CFGNode> startNodes = controlFlowGraph.startNodes();
-        final Set.Immutable<CFGNode> endNodes = controlFlowGraph.endNodes();
-        final Set.Immutable<CFGNode> normalNodes = controlFlowGraph.normalNodes();
-        final Set.Immutable<CFGNode> nodes = Set.Immutable.union(Set.Immutable.union(startNodes, endNodes), normalNodes);
+        final Set.Immutable<CFGNode> entryExitNodes = Set.Immutable.union(controlFlowGraph.entryNodes(), controlFlowGraph.exitNodes());
 
-        final String starts = startNodes.stream().map(node -> nodeToDot(node, properties)).collect(Collectors.joining());
-        final String ends = endNodes.stream().map(node -> nodeToDot(node, properties)).collect(Collectors.joining());
-        final String otherNodes = normalNodes.stream().map(node -> nodeToDot(node, properties)).collect(Collectors.joining());
-        final String edges = controlFlowGraph.edges().tupleStream(ImmutableTuple2::of)
-            .filter(t -> nodes.contains(t._1()) && nodes.contains(t._2())).map(this::edgeToDot)
-            .collect(Collectors.joining());
+        final String starts = controlFlowGraph.startNodes().stream().map(node -> nodeToDot(node, properties)).collect(Collectors.joining());
+        final String ends = controlFlowGraph.endNodes().stream().map(node -> nodeToDot(node, properties)).collect(Collectors.joining());
+        final String otherNodes = controlFlowGraph.normalNodes().stream().map(node -> nodeToDot(node, properties)).collect(Collectors.joining());
+        final String edges = removeNodes(controlFlowGraph.edges(), entryExitNodes).tupleStream(this::edgeToDot).collect(Collectors.joining());
 
         return "digraph FG {\n"
              + "node [ shape = record, style = \"rounded\" ];\n"
@@ -50,8 +46,24 @@ public final class ControlFlowGraphTerms {
              + "}";
     }
 
-    private String edgeToDot(ImmutableTuple2<CFGNode, CFGNode> t) {
-        return "\"" + t._1().toString() + "\" -> \"" + t._2().toString() + "\";\n";
+    private BinaryRelation.Immutable<CFGNode, CFGNode> removeNodes(BinaryRelation.Immutable<CFGNode, CFGNode> edges, Set.Immutable<CFGNode> toRemove) {
+        BinaryRelation.Transient<CFGNode, CFGNode> result = edges.asTransient();
+        for(CFGNode node : toRemove) {
+            Set.Immutable<CFGNode> tos = result.get(node);
+            Set.Immutable<CFGNode> froms = result.inverse().get(node);
+            result.__remove(node); // remove node ->> tos
+            for(CFGNode from : froms) {
+                result.__remove(from, node); // remove froms ->> node
+                for(CFGNode to : tos) {
+                    result.__put(from, to); // add froms -->> tos
+                }
+            }
+        }
+        return result.freeze();
+    }
+
+    private String edgeToDot(CFGNode from, CFGNode to) {
+        return "\"" + from.toString() + "\" -> \"" + to.toString() + "\";\n";
     }
 
     private String nodeToDot(CFGNode node, java.util.Set<String> properties) {
