6db5f4b1c6ce06751cbacee1b7df3dcff988db70
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-03-05 13:06:20 +0100

    Move nested class to separate file to prevent duplicate definition errors.

--- nabl2.terms/src/test/java/mb/nabl2/terms/build/HashcodeAndEqualsTest.java
+++ nabl2.terms/src/test/java/mb/nabl2/terms/build/HashcodeAndEqualsTest.java
@@ -11,16 +11,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
-import java.util.List;
 
-import org.immutables.serial.Serial;
-import org.immutables.value.Value;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.collect.ImmutableList;
 
-import mb.nabl2.terms.IApplTerm;
 import mb.nabl2.terms.ITerm;
 
 public class HashcodeAndEqualsTest {
@@ -158,46 +154,6 @@ public class HashcodeAndEqualsTest {
         assertTrue(Arrays.equals(b1, b2));
     }
 
-
-    @Value.Immutable
-    @Serial.Version(42L)
-    static abstract class SpecializedAppl extends AbstractApplTerm {
-
-        static final String OP = "Specialized";
-
-        @Value.Parameter public abstract String getFirstArg();
-
-        @Value.Parameter public abstract int getSecondArg();
-
-        @Override public String getOp() {
-            return OP;
-        }
-
-        @Override public List<ITerm> getArgs() {
-            return ImmutableList.of(B.newString(getFirstArg()), B.newInt(getSecondArg()));
-        }
-
-        @Override public IApplTerm withAttachments(ImmutableClassToInstanceMap<Object> value) {
-            return ImmutableSpecializedAppl.copyOf(this).withAttachments(value);
-        }
-
-        @Override protected SpecializedAppl check() {
-            return this;
-        }
-
-        @Override public int hashCode() {
-            return super.hashCode();
-        }
-
-        @Override public boolean equals(Object other) {
-            return super.equals(other);
-        }
-
-        @Override public String toString() {
-            return getFirstArg() + ":" + getSecondArg();
-        }
-
-    }
 
     private static byte[] serialize(Object obj) throws IOException {
         try(final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
