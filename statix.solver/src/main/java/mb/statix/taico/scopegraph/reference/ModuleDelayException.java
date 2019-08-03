9c6af4d46c3e291b3ebd6a9a738fae14e4757d79
Taico Aerts <devtaeir@taico.nl>
2019-08-03 14:58:36 +0200

    Add requester to ModuleDelayException

--- statix.solver/src/main/java/mb/statix/taico/scopegraph/reference/ModuleDelayException.java
+++ statix.solver/src/main/java/mb/statix/taico/scopegraph/reference/ModuleDelayException.java
@@ -1,14 +1,24 @@
 package mb.statix.taico.scopegraph.reference;
 
+import java.util.Optional;
+
 public class ModuleDelayException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
     private final String module;
+    private final String requester;
 
     public ModuleDelayException(String module) {
-        super("Incomplete module " + module);
+        super("Incomplete module \"" + module + "\"");
         this.module = module;
+        this.requester = null;
+    }
+    
+    public ModuleDelayException(String requester, String module) {
+        super("Module \"" + module + "\" is incomplete or inaccessible from module \"" + requester + "\"");
+        this.module = module;
+        this.requester = requester;
     }
     
     /**
@@ -17,5 +27,9 @@ public class ModuleDelayException extends RuntimeException {
      */
     public String getModule() {
         return module;
+    }
+    
+    public Optional<String> getRequester() {
+        return Optional.of(requester);
     }
 }
