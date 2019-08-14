6c6c2ba3b4703bd615c1d3d4bca4eef130572d0a
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-08-14 18:41:28 +0200

    Add utility functions.

---
+++ statix.lang/src/main/strategies/statix/lang/strategies/InteropRegisterer.java
@@ -0,0 +1,14 @@
+package statix.lang.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+public class InteropRegisterer extends JavaInteropRegisterer {
+    public InteropRegisterer() {
+        // @formatter:off
+        super(new Strategy[] {
+            group_by_2_0.instance
+        });
+        // @formatter:on
+    }
+}
---
+++ statix.lang/src/main/strategies/statix/lang/strategies/Main.java
@@ -0,0 +1,10 @@
+package statix.lang.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {
+
+    public static void init(Context context) {
+    }
+
+}
