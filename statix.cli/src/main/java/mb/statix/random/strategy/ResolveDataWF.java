3662d1813f07dd94262372a055e9b1d101986060
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-09-17 13:39:49 +0200

    Follow Solver::entails, and make note about that.

--- statix.cli/src/main/java/mb/statix/random/strategy/ResolveDataWF.java
+++ statix.cli/src/main/java/mb/statix/random/strategy/ResolveDataWF.java
@@ -4,9 +4,12 @@ import static mb.nabl2.terms.build.TermBuild.B;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.oracle.truffle.api.object.dsl.Nullable;
 
 import mb.nabl2.terms.ITerm;
@@ -46,11 +49,13 @@ class ResolveDataWF implements DataWF<ITerm, CEqual> {
         if(!stateAndConstraint.isPresent()) {
             return Optional.empty();
         }
-        final IState.Immutable newState = stateAndConstraint.get()._1();
-        final IConstraint constraint = stateAndConstraint.get()._2();
 
+        // NOTE This method is almost a duplicate of Solver::entails and should be
+        //      kept in sync
+        
         // solve rule constraint
-        final SolverResult result = Solver.solve(newState, constraint, isComplete3, new NullDebugContext());
+        final SolverResult result = Solver.solve(stateAndConstraint.get()._1(), stateAndConstraint.get()._2(),
+                isComplete3, new NullDebugContext());
         if(result.hasErrors()) {
             return Optional.empty();
         }
@@ -58,12 +63,34 @@ class ResolveDataWF implements DataWF<ITerm, CEqual> {
             return Optional.empty();
         }
 
-        final List<ITerm> leftTerms = Lists.newArrayList();
-        final List<ITerm> rightTerms = Lists.newArrayList();
+        final IState.Immutable newState = result.state();
         // NOTE The retain operation is important because it may change
         //      representatives, which can be local to newUnifier.
-        final IUnifier.Immutable newUnifier = result.state().unifier().retainAll(state.vars()).unifier();
-        for(ITermVar var : state.vars()) {
+        final IUnifier.Immutable newUnifier = newState.unifier().retainAll(state.vars()).unifier();
+
+        final List<ITermVar> disunifiedVars =
+                Sets.difference(newUnifier.disequalityMaps(), state.unifier().disequalityMaps()).stream()
+                        .flatMap(diseq -> diseq.keySet().stream()).collect(Collectors.toList());
+        // FIXME Any disequalities on rigid vars introduced during solving are
+        //       currently ignored. We could test as follows. Remove all disequalities
+        //       in state from newState. Any remaining disequalities are
+        //       (a) reductions of diseq in state, or
+        //       (b) new diseq.
+        //       In case (a), this can only happen if a rigid var was unified, so
+        //       this is caught by the previous check. In case (b), this is a new
+        //       inequality, and entailment should not hold. It is a little more
+        //       complicated than that, because, if the new diseq in newState is
+        //       implied by all diseq in state, entailment does hold. But it might
+        //       okay to ignore this situation?
+        if(!disunifiedVars.isEmpty()) {
+            return Optional.empty();
+        }
+
+        final Set<ITermVar> unifiedVars = Sets.difference(newUnifier.varSet(), state.unifier().varSet());
+        // FIXME This test assumes the newUnifier is an extension of the old one.
+        final List<ITerm> leftTerms = Lists.newArrayList();
+        final List<ITerm> rightTerms = Lists.newArrayList();
+        for(ITermVar var : unifiedVars) {
             final ITerm term = newUnifier.findTerm(var);
             if(!state.unifier().areEqual(var, term).orElse(false)) {
                 leftTerms.add(var);
