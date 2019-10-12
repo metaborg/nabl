8d3cd9fdcf80754da59e42ca021dd46f7e4b5b5a
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-10-12 13:18:39 +0200

    Revert to original unifier since incremental version is still buggy.

--- nabl2.terms/src/main/java/mb/nabl2/terms/unification/Unifiers.java
+++ nabl2.terms/src/main/java/mb/nabl2/terms/unification/Unifiers.java
@@ -5,13 +5,13 @@ public class Unifiers {
     public static class Immutable {
 
         public static IUnifier.Immutable of() {
-//            return PersistentUnifier.Immutable.of();
-            return IncrementalVarSetPersistentUnifier.Immutable.of();
+            return PersistentUnifier.Immutable.of();
+//            return IncrementalVarSetPersistentUnifier.Immutable.of();
         }
 
         public static IUnifier.Immutable of(boolean finite) {
-//            return PersistentUnifier.Immutable.of(finite);
-            return IncrementalVarSetPersistentUnifier.Immutable.of(finite);
+            return PersistentUnifier.Immutable.of(finite);
+//            return IncrementalVarSetPersistentUnifier.Immutable.of(finite);
         }
 
     }
