589540d954eeb2c0fb186bb95b86cbc877767347
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-10-15 21:13:06 +0200

    Prevent stack overflow on very deep conjunctions.

--- statix.solver/src/main/java/mb/statix/solver/completeness/CompletenessUtil.java
+++ statix.solver/src/main/java/mb/statix/solver/completeness/CompletenessUtil.java
@@ -3,10 +3,12 @@ package mb.statix.solver.completeness;
 import static mb.nabl2.terms.matching.TermMatch.M;
 
 import java.util.Collection;
+import java.util.Deque;
 
 import org.metaborg.util.functions.Action2;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
 import mb.nabl2.terms.ITerm;
 import mb.nabl2.terms.matching.TermMatch.IMatcher;
@@ -23,46 +25,50 @@ public class CompletenessUtil {
      * Discover critical edges in constraint. The scopeTerm is not guaranteed to be ground or instantiated.
      */
     static void criticalEdges(IConstraint constraint, Spec spec, Action2<ITerm, ITerm> criticalEdge) {
-        // @formatter:off
-        constraint.match(Constraints.cases(
-            onArith -> null,
-            onConj -> {
-                criticalEdges(onConj.left(), spec, criticalEdge);
-                criticalEdges(onConj.right(), spec, criticalEdge);
-                return null;
-            },
-            onEqual -> null,
-            onExists -> {
-                criticalEdges(onExists.constraint(), spec, (s, l) -> {
-                    if(!onExists.vars().contains(s)) {
-                        criticalEdge.apply(s, l);
-                    }
-                });
-                return null;
-            },
-            onFalse -> null,
-            onInequal -> null,
-            onNew -> null,
-            onResolveQuery -> null,
-            onTellEdge -> {
-                criticalEdge.apply(onTellEdge.sourceTerm(), onTellEdge.label());
-                return null;
-            },
-            onTellRel -> {
-                criticalEdge.apply(onTellRel.scopeTerm(), onTellRel.relation());
-                return null;
-            },
-            onTermId -> null,
-            onTermProperty -> null,
-            onTrue -> null,
-            onTry -> null,
-            onUser -> {
-                spec.scopeExtensions().get(onUser.name()).stream()
-                        .forEach(il -> criticalEdge.apply(onUser.args().get(il._1()), il._2()));
-                return null;
-            }
-        ));
-        // @formatter:on
+        final Deque<IConstraint> worklist = Lists.newLinkedList();
+        worklist.push(constraint);
+        while(!worklist.isEmpty()) {
+            // @formatter:off
+            worklist.pop().match(Constraints.cases(
+                onArith -> null,
+                onConj -> {
+                    worklist.push(onConj.left());
+                    worklist.push(onConj.right());
+                    return null;
+                },
+                onEqual -> null,
+                onExists -> {
+                    criticalEdges(onExists.constraint(), spec, (s, l) -> {
+                        if(!onExists.vars().contains(s)) {
+                            criticalEdge.apply(s, l);
+                        }
+                    });
+                    return null;
+                },
+                onFalse -> null,
+                onInequal -> null,
+                onNew -> null,
+                onResolveQuery -> null,
+                onTellEdge -> {
+                    criticalEdge.apply(onTellEdge.sourceTerm(), onTellEdge.label());
+                    return null;
+                },
+                onTellRel -> {
+                    criticalEdge.apply(onTellRel.scopeTerm(), onTellRel.relation());
+                    return null;
+                },
+                onTermId -> null,
+                onTermProperty -> null,
+                onTrue -> null,
+                onTry -> null,
+                onUser -> {
+                    spec.scopeExtensions().get(onUser.name()).stream()
+                            .forEach(il -> criticalEdge.apply(onUser.args().get(il._1()), il._2()));
+                    return null;
+                }
+            ));
+            // @formatter:on
+        }
     }
 
     /**
