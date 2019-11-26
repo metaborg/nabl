2ce2936f0b521c3326329c6acea99a38dc52ff5f
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-11-26 14:53:51 +0100

    Improve fixed-point equations.

--- statix.lang/src/main/strategies/statix/lang/strategies/set_fixed_point_0_1.java
+++ statix.lang/src/main/strategies/statix/lang/strategies/set_fixed_point_0_1.java
@@ -1,6 +1,7 @@
 package statix.lang.strategies;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,10 +34,7 @@ public class set_fixed_point_0_1 extends Strategy {
             final IStrategoTerm base) {
         final ITermFactory TF = context.getFactory();
 
-        if(!Tools.isTermList(base)) {
-            throw new java.lang.IllegalArgumentException("Expected base set, got " + base);
-        }
-        final Set.Immutable<IStrategoTerm> baseSet = CapsuleUtil.toSet(Arrays.asList(base.getAllSubterms()));
+        final Set.Immutable<IStrategoTerm> baseSet = termToSet(base);
 
         if(!Tools.isTermList(current)) {
             throw new java.lang.IllegalArgumentException("Expected list of equations, got " + current);
@@ -45,21 +43,23 @@ public class set_fixed_point_0_1 extends Strategy {
         final Map<IStrategoTerm, Set.Immutable<IStrategoTerm>> values = Maps.newHashMap();
         for(IStrategoTerm varEq : current.getAllSubterms()) {
             if(!Tools.isTermTuple(varEq) || varEq.getSubtermCount() != 2) {
-                throw new java.lang.IllegalArgumentException("Expected tuple of variable and components, got " + varEq);
+                throw new java.lang.IllegalArgumentException(
+                        "Expected triple of variable, init, and components, got " + varEq);
             }
             final IStrategoTerm var = varEq.getSubterm(0);
-            final IStrategoTerm eq = varEq.getSubterm(1);
-            eqs.put(var, parse(eq));
+            final Eq eq = parse(varEq.getSubterm(1));
+            eqs.put(var, eq);
             values.put(var, baseSet);
         }
-
 
         boolean again = true;
         while(again) {
             again = false;
-            for(IStrategoTerm var : eqs.keySet()) {
-                final Set.Immutable<IStrategoTerm> varResult = eqs.get(var).apply(values);
-                final Set.Immutable<IStrategoTerm> prevResult = values.put(var, varResult);
+            List<Map.Entry<IStrategoTerm, Eq>> _eqs = Lists.newArrayList(eqs.entrySet());
+            Collections.shuffle(_eqs);
+            for(Map.Entry<IStrategoTerm, Eq> varEq : _eqs) {
+                final Set.Immutable<IStrategoTerm> varResult = varEq.getValue().apply(values);
+                final Set.Immutable<IStrategoTerm> prevResult = values.put(varEq.getKey(), varResult);
                 again |= prevResult == null || !prevResult.equals(varResult);
             }
         }
@@ -78,23 +78,34 @@ public class set_fixed_point_0_1 extends Strategy {
         if(!Tools.isTermAppl(term)) {
             throw new IllegalArgumentException("Expected equation, got " + term);
         }
-        if(Tools.hasConstructor((IStrategoAppl) term, "Union", 1)) {
-            final IStrategoTerm components = term.getSubterm(0);
+        if(Tools.hasConstructor((IStrategoAppl) term, "Union", 2)) {
+            final Set.Immutable<IStrategoTerm> baseSet = termToSet(term.getSubterm(0));
+            final IStrategoTerm components = term.getSubterm(1);
             if(!Tools.isTermList(components) || components.getSubtermCount() == 0) {
                 throw new IllegalArgumentException("Expected equations, got " + components);
             }
-            return new Union(Streams.stream(components).map(this::parse).collect(ImmutableList.toImmutableList()));
-        } else if(Tools.hasConstructor((IStrategoAppl) term, "Intersection", 1)) {
-            final IStrategoTerm components = term.getSubterm(0);
+            return new Union(baseSet,
+                    Streams.stream(components).map(t -> parse(t)).collect(ImmutableList.toImmutableList()));
+        } else if(Tools.hasConstructor((IStrategoAppl) term, "Intersection", 2)) {
+            final Set.Immutable<IStrategoTerm> baseSet = termToSet(term.getSubterm(0));
+            final IStrategoTerm components = term.getSubterm(1);
             if(!Tools.isTermList(components) || components.getSubtermCount() == 0) {
                 throw new IllegalArgumentException("Expected equations, got " + components);
             }
-            return new Intersection(
-                    Streams.stream(components).map(this::parse).collect(ImmutableList.toImmutableList()));
+            return new Intersection(baseSet,
+                    Streams.stream(components).map(t -> parse(t)).collect(ImmutableList.toImmutableList()));
         } else {
             return new Var(term);
         }
     }
+
+    private Set.Immutable<IStrategoTerm> termToSet(IStrategoTerm term) {
+        if(!Tools.isTermList(term)) {
+            throw new java.lang.IllegalArgumentException("Expected base set, got " + term);
+        }
+        return CapsuleUtil.toSet(Arrays.asList(term.getAllSubterms()));
+    }
+
 
     private abstract class Eq {
 
@@ -104,14 +115,16 @@ public class set_fixed_point_0_1 extends Strategy {
 
     private class Union extends Eq {
 
+        private final Set.Immutable<IStrategoTerm> base;
         private final List<Eq> components;
 
-        public Union(Iterable<Eq> components) {
+        public Union(Set.Immutable<IStrategoTerm> base, Iterable<Eq> components) {
+            this.base = base;
             this.components = ImmutableList.copyOf(components);
         }
 
         @Override public Set.Immutable<IStrategoTerm> apply(Map<IStrategoTerm, Set.Immutable<IStrategoTerm>> values) {
-            return components.stream().map(c -> c.apply(values)).reduce((s1, s2) -> Set.Immutable.union(s1, s2)).get();
+            return components.stream().map(c -> c.apply(values)).reduce(base, (s1, s2) -> Set.Immutable.union(s1, s2));
         }
 
         @Override public String toString() {
@@ -123,15 +136,17 @@ public class set_fixed_point_0_1 extends Strategy {
 
     private class Intersection extends Eq {
 
+        private final Set.Immutable<IStrategoTerm> base;
         private final List<Eq> components;
 
-        public Intersection(Iterable<Eq> components) {
+        public Intersection(Set.Immutable<IStrategoTerm> base, Iterable<Eq> components) {
+            this.base = base;
             this.components = ImmutableList.copyOf(components);
         }
 
         @Override public Set.Immutable<IStrategoTerm> apply(Map<IStrategoTerm, Set.Immutable<IStrategoTerm>> values) {
-            return components.stream().map(c -> c.apply(values)).reduce((s1, s2) -> Set.Immutable.intersect(s1, s2))
-                    .get();
+            return components.stream().map(c -> c.apply(values)).reduce(base,
+                    (s1, s2) -> Set.Immutable.intersect(s1, s2));
         }
 
         @Override public String toString() {
