b6c92a4b1d3d847ec51b2b0d5992cc82da7e0fa6
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2020-03-20 10:51:13 +0100

    Factor out strategy call logic into separate utility class.

--- nabl2.solver/src/main/java/mb/nabl2/spoofax/primitives/ScopeGraphMultiFileAnalysisPrimitive.java
+++ nabl2.solver/src/main/java/mb/nabl2/spoofax/primitives/ScopeGraphMultiFileAnalysisPrimitive.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.metaborg.util.functions.PartialFunction1;
 import org.metaborg.util.log.ILogger;
 import org.metaborg.util.log.LoggerUtils;
 import org.metaborg.util.task.ICancel;
@@ -16,18 +15,16 @@ import org.metaborg.util.task.NullProgress;
 import org.metaborg.util.task.ThreadCancel;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;
-import org.spoofax.interpreter.core.UndefinedStrategyException;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.stratego.SDefT;
 import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
-import org.strategoxt.HybridInterpreter;
-import org.strategoxt.lang.InteropContext;
 
 import mb.nabl2.config.NaBL2DebugConfig;
 import mb.nabl2.solver.solvers.CallExternal;
 import mb.nabl2.solver.solvers.SemiIncrementalMultiFileSolver;
+import mb.nabl2.spoofax.primitives.StrategyCalls.CallableStrategy;
 import mb.nabl2.stratego.ConstraintTerms;
 import mb.nabl2.terms.ITerm;
 import mb.nabl2.terms.stratego.StrategoTerms;
@@ -72,8 +69,8 @@ public abstract class ScopeGraphMultiFileAnalysisPrimitive extends AbstractPrimi
         return (name, args) -> {
             final IStrategoTerm arg = prepareArguments(args, strategoTerms, env.getFactory());
             try {
-                PartialFunction1<IStrategoTerm, IStrategoTerm> evalStrategy = callStrategy(env, strCache, name);
-                final Optional<IStrategoTerm> result = evalStrategy.apply(arg);
+                final CallableStrategy strategy = StrategyCalls.lookup(env, name, strCache);
+                final Optional<IStrategoTerm> result = strategy.call(arg);
                 return result.map(strategoTerms::fromStratego).map(ConstraintTerms::specialize);
             } catch(Exception ex) {
                 logger.warn("External call to '{}' failed.", ex, name);
@@ -81,67 +78,6 @@ public abstract class ScopeGraphMultiFileAnalysisPrimitive extends AbstractPrimi
             }
         };
 
-    }
-
-    private static PartialFunction1<IStrategoTerm, IStrategoTerm> callStrategy(IContext env,
-            HashMap<String, SDefT> strCache, String name) throws InterpreterException {
-        final String cname = name.replace('-', '_') + "_0_0";
-        if(env instanceof InteropContext) {
-            return callStrategyJar((InteropContext) env, name);
-        } else {
-            return callStrategyCtree(env, strCache, name, cname);
-        }
-    }
-
-    private static PartialFunction1<IStrategoTerm, IStrategoTerm> callStrategyJar(InteropContext context, String name) {
-        final HybridInterpreter interpreter = HybridInterpreter.getInterpreter(context.getContext());
-        return arg -> {
-            final IStrategoTerm prev = interpreter.current();
-            try {
-                interpreter.setCurrent(arg);
-                if(!interpreter.invoke(name)) {
-                    return Optional.empty();
-                }
-                return Optional.of(interpreter.current());
-            } catch(UndefinedStrategyException ex) {
-                logger.warn("Cannot find strategy '{}'", name);
-                return Optional.empty();
-            } catch(InterpreterException ex) {
-                logger.warn("External call to '{}' failed.", ex, name);
-                return Optional.empty();
-            } finally {
-                interpreter.setCurrent(prev);
-            }
-        };
-    }
-
-    private static PartialFunction1<IStrategoTerm, IStrategoTerm> callStrategyCtree(IContext env,
-            HashMap<String, SDefT> strCache, String name, final String cname) throws InterpreterException {
-        final SDefT s;
-        if(strCache.containsKey(name)) {
-            s = strCache.get(name);
-        } else {
-            s = env.lookupSVar(cname);
-            if(s == null) {
-                logger.warn("Cannot find strategy '{}'", name);
-                return arg -> Optional.empty();
-            }
-            strCache.put(name, s);
-        }
-        return arg -> {
-            final IStrategoTerm prev = env.current();
-            try {
-                if(!s.evaluate(env)) {
-                    return Optional.empty();
-                }
-                return Optional.of(env.current());
-            } catch(InterpreterException ex) {
-                logger.warn("External call to '{}' failed.", ex, name);
-                return Optional.empty();
-            } finally {
-                env.setCurrent(prev);
-            }
-        };
     }
 
     private static IStrategoTerm prepareArguments(Collection<? extends ITerm> args, StrategoTerms strategoTerms,
---
+++ nabl2.solver/src/main/java/mb/nabl2/spoofax/primitives/StrategyCalls.java
@@ -0,0 +1,85 @@
+package mb.nabl2.spoofax.primitives;
+
+import java.util.Map;
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.InterpreterException;
+import org.spoofax.interpreter.core.UndefinedStrategyException;
+import org.spoofax.interpreter.stratego.SDefT;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.strategoxt.HybridInterpreter;
+import org.strategoxt.lang.InteropContext;
+
+public class StrategyCalls {
+
+    public static CallableStrategy lookup(IContext env, String name) throws InterpreterException {
+        return lookup(env, name, null);
+    }
+
+    public static CallableStrategy lookup(IContext env, String name, @Nullable Map<String, SDefT> strCache)
+            throws InterpreterException {
+        if(env instanceof InteropContext) {
+            return lookupStrategyJar((InteropContext) env, name);
+        } else {
+            return lookupStrategyCtree(env, name, strCache);
+        }
+    }
+
+    private static CallableStrategy lookupStrategyJar(InteropContext context, String name) throws InterpreterException {
+        final HybridInterpreter interpreter = HybridInterpreter.getInterpreter(context.getContext());
+        if(interpreter == null) {
+            throw new InterpreterException("Cannot get interpreter.");
+        }
+        return arg -> {
+            final IStrategoTerm prev = interpreter.current();
+            interpreter.setCurrent(arg);
+            try {
+                if(!interpreter.invoke(name)) {
+                    return Optional.empty();
+                }
+                return Optional.of(interpreter.current());
+            } finally {
+                interpreter.setCurrent(prev);
+            }
+        };
+    }
+
+    private static CallableStrategy lookupStrategyCtree(IContext env, final String name,
+            @Nullable Map<String, SDefT> strCache) throws InterpreterException {
+        final String cname = name.replace('-', '_') + "_0_0";
+        final SDefT s;
+        if(strCache != null && strCache.containsKey(cname)) {
+            s = strCache.get(cname);
+        } else {
+            s = env.lookupSVar(cname);
+            if(s == null) {
+                throw new UndefinedStrategyException("Strategy lookup failed.", name);
+            }
+            if(strCache != null) {
+                strCache.put(cname, s);
+            }
+        }
+        return arg -> {
+            final IStrategoTerm prev = env.current();
+            env.setCurrent(arg);
+            try {
+                if(!s.evaluate(env)) {
+                    return Optional.empty();
+                }
+                return Optional.of(env.current());
+            } finally {
+                env.setCurrent(prev);
+            }
+        };
+    }
+
+    public interface CallableStrategy {
+
+        Optional<IStrategoTerm> call(IStrategoTerm args) throws InterpreterException;
+
+    }
+
+}
