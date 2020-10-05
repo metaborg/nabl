ccbd048de02c268d18daa57bbcee24ed22de98b1
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2020-10-05 16:55:25 +0200

    Remove dependencie from Gradle file as well.

--- statix.solver/build.gradle.kts
+++ statix.solver/build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
 
   api("com.google.guava:guava")
   api("io.usethesource:capsule")
-  api("org.eclipse.viatra:org.eclipse.viatra.query.runtime.base.itc:2.3.2")
   compileOnly("com.google.code.findbugs:jsr305")
 
   // Annotation processing
