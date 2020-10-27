9c0ecba02732e03590f6a6a6f0724662ab53a49d
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2020-10-27 01:12:55 +0100

    Add all benchmarks and tweak Maven exec goals.

--- statix.benchmarks/src/main/java/mb/statix/benchmarks/CommonsCSVBenchmark.java
+++ statix.benchmarks/src/main/java/mb/statix/benchmarks/CommonsCSVBenchmark.java
@@ -1,20 +1,52 @@
 package mb.statix.benchmarks;
 
+import java.util.concurrent.TimeUnit;
+
 import org.metaborg.util.log.ILogger;
 import org.metaborg.util.log.LoggerUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
 
 import mb.nabl2.terms.ITerm;
 import mb.statix.spec.Rule;
 import mb.statix.spec.Spec;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(1)
+@Threads(1)
+@State(Scope.Thread)
 public class CommonsCSVBenchmark {
 
     private static final ILogger logger = LoggerUtils.logger(CommonsCSVBenchmark.class);
+
+    @Param({ "1", "2", "4", "8" }) public int parallelism;
+
+    private Spec spec;
+    private java.util.Map<String, Rule> units;
+
+    @Setup(Level.Trial) public void doSetup() {
+        try {
+            this.spec = BenchmarkUtil.loadSpecFromATerm("metaborg-java.mergedspec.aterm");
+            java.util.Map<String, ITerm> files = BenchmarkUtil.loadFilesFromATerm("commons-csv.files.aterm");
+            this.units = BenchmarkUtil.makeFileUnits(files, "statics!projectOk", "statics!fileOk");
+        } catch(Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+    @Benchmark public Object run() throws Exception, InterruptedException {
+        return BenchmarkUtil.runConcurrent(spec, units, parallelism);
+    }
 
     public static void main(String[] args) throws Exception, InterruptedException {
         Spec spec = BenchmarkUtil.loadSpecFromATerm("metaborg-java.mergedspec.aterm");
@@ -25,29 +57,6 @@ public class CommonsCSVBenchmark {
         BenchmarkUtil.runConcurrent(spec, units, parallelism);
         final long dt = System.currentTimeMillis() - t0;
         logger.info("Finished after {} s.", dt / 1_000d);
-    }
-
-    @State(Scope.Thread)
-    public static class Input {
-
-        Spec spec;
-        java.util.Map<String, Rule> units;
-        int parallelism = Runtime.getRuntime().availableProcessors();
-
-        public Input() {
-            try {
-                this.spec = BenchmarkUtil.loadSpecFromATerm("metaborg-java.mergedspec.aterm");
-                java.util.Map<String, ITerm> files = BenchmarkUtil.loadFilesFromATerm("commons-csv.files.aterm");
-                this.units = BenchmarkUtil.makeFileUnits(files, "statics!projectOk", "statics!fileOk");
-            } catch(Throwable t) {
-                throw new RuntimeException(t);
-            }
-        }
-
-    }
-
-    @Benchmark @BenchmarkMode(Mode.All) public Object run(Input input) throws Exception, InterruptedException {
-        return BenchmarkUtil.runConcurrent(input.spec, input.units, input.parallelism);
     }
 
 }
--- statix.benchmarks/src/main/java/mb/statix/benchmarks/JDK8ReducedBenchmark.java
+++ statix.benchmarks/src/main/java/mb/statix/benchmarks/JDK8ReducedBenchmark.java
@@ -1,15 +1,52 @@
 package mb.statix.benchmarks;
 
+import java.util.concurrent.TimeUnit;
+
 import org.metaborg.util.log.ILogger;
 import org.metaborg.util.log.LoggerUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
 
 import mb.nabl2.terms.ITerm;
 import mb.statix.spec.Rule;
 import mb.statix.spec.Spec;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(1)
+@Threads(1)
+@State(Scope.Thread)
 public class JDK8ReducedBenchmark {
 
     private static final ILogger logger = LoggerUtils.logger(JDK8ReducedBenchmark.class);
+
+    @Param({ "1", "2", "4", "8" }) public int parallelism;
+
+    private Spec spec;
+    private java.util.Map<String, Rule> units;
+
+    @Setup(Level.Trial) public void doSetup() {
+        try {
+            this.spec = BenchmarkUtil.loadSpecFromATerm("metaborg-java.mergedspec.aterm");
+            java.util.Map<String, ITerm> files = BenchmarkUtil.loadFilesFromATerm("jd8_reduced.files.aterm");
+            this.units = BenchmarkUtil.makeFileUnits(files, "statics!projectOk", "statics!fileOk");
+        } catch(Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+    @Benchmark public Object run() throws Exception, InterruptedException {
+        return BenchmarkUtil.runConcurrent(spec, units, parallelism);
+    }
 
     public static void main(String[] args) throws Exception, InterruptedException {
         Spec spec = BenchmarkUtil.loadSpecFromATerm("metaborg-java.mergedspec.aterm");
