186456602586f2852dc4c724bb725c8907b5262b
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2020-10-30 22:49:57 +0100

    Adapt benchmark to allow executing as application.

--- statix.benchmarks/src/main/java/mb/statix/benchmarks/actors/ProducerConsumerBenchmark.java
+++ statix.benchmarks/src/main/java/mb/statix/benchmarks/actors/ProducerConsumerBenchmark.java
@@ -2,6 +2,8 @@ package mb.statix.benchmarks.actors;
 
 import java.util.concurrent.TimeUnit;
 
+import org.metaborg.util.log.ILogger;
+import org.metaborg.util.log.LoggerUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -27,18 +29,24 @@ import mb.statix.concurrent.actors.impl.ActorSystem;
 @State(Scope.Thread)
 public class ProducerConsumerBenchmark {
 
+    private static final ILogger logger = LoggerUtils.logger(ProducerConsumerBenchmark.class);
+
     @Param({ "1", "2", "4", "8" }) public int parallelism;
     @Param({ "0", "8", "32", "128" }) public int producers;
     @Param({ "1024", "8192", "65536", "262144" }) public int messages;
 
     @Benchmark public Object run() throws Exception, InterruptedException {
+        return doRun(producers, messages, parallelism);
+    }
+
+    private static Integer doRun(int producers, int messages, int parallelism) throws Exception, InterruptedException {
         final ActorSystem actorSystem = new ActorSystem(parallelism);
         final ICompletableFuture<Integer> result = new CompletableFuture<>();
-        final IActor<IConsumer> consumer =
-                actorSystem.add("consumer", TypeTag.of(IConsumer.class), self -> new Consumer(self, result));
+        final IActor<IConsumer> consumer = actorSystem.add("consumer", TypeTag.of(IConsumer.class),
+                self -> new Consumer(self, producers * messages, result));
         for(int i = 0; i < producers; i++) {
-            final IActor<IProducer> producer =
-                    actorSystem.add("producer-" + i, TypeTag.of(IProducer.class), self -> new Producer(self, consumer));
+            final IActor<IProducer> producer = actorSystem.add("producer-" + i, TypeTag.of(IProducer.class),
+                    self -> new Producer(self, consumer, messages));
             actorSystem.async(producer).sentAll();
         }
         actorSystem.start();
@@ -55,14 +63,16 @@ public class ProducerConsumerBenchmark {
         void receive();
     }
 
-    private class Producer implements IProducer {
+    private static class Producer implements IProducer {
 
         private final IActor<IProducer> self;
         private final IActorRef<IConsumer> consumer;
+        private final int messages;
 
-        public Producer(IActor<IProducer> self, IActorRef<IConsumer> consumer) {
+        public Producer(IActor<IProducer> self, IActorRef<IConsumer> consumer, int messages) {
             this.self = self;
             this.consumer = consumer;
+            this.messages = messages;
         }
 
         @Override public void sentAll() {
@@ -74,12 +84,14 @@ public class ProducerConsumerBenchmark {
 
     }
 
-    private class Consumer implements IConsumer {
+    private static class Consumer implements IConsumer {
 
+        private final int expected;
         private final ICompletable<Integer> result;
         private int received = 0;
 
-        public Consumer(IActor<IConsumer> self, ICompletable<Integer> result) {
+        public Consumer(IActor<IConsumer> self, int expected, ICompletable<Integer> result) {
+            this.expected = expected;
             this.result = result;
             tryFinish();
         }
@@ -90,11 +102,21 @@ public class ProducerConsumerBenchmark {
         }
 
         private void tryFinish() {
-            if(received >= (producers * messages)) {
+            if(received >= expected) {
                 result.complete(received);
             }
         }
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            while(true) {
+                doRun(0, 1024, 1);
+            }
+        } catch(Exception ex) {
+            logger.error("Failed.", ex);
+        }
     }
 
 }
