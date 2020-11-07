eb49882d8d65528bd408f9c810d65b435e1a3659
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2020-11-07 16:41:52 +0100

    Give variables a default ordering.

--- nabl2.terms/src/main/java/mb/nabl2/terms/ITermVar.java
+++ nabl2.terms/src/main/java/mb/nabl2/terms/ITermVar.java
@@ -1,12 +1,22 @@
 package mb.nabl2.terms;
 
-public interface ITermVar extends ITerm, IListTerm {
+public interface ITermVar extends ITerm, IListTerm, Comparable<ITermVar> {
 
     String getResource();
 
     String getName();
 
-    @Override
-    ITermVar withAttachments(IAttachments value);
+    @Override ITermVar withAttachments(IAttachments value);
+
+    @Override default int compareTo(ITermVar other) {
+        int c = 0;
+        if(c == 0) {
+            c = getResource().compareTo(other.getResource());
+        }
+        if(c == 0) {
+            c = getName().compareTo(other.getName());
+        }
+        return c;
+    }
 
 }
