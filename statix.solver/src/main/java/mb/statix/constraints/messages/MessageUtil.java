d878ba9ffeb104bd4c89553ca3b5ba1a31985c15
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-11-11 11:12:47 +0100

    Fix MessageKind

--- statix.solver/src/main/java/mb/statix/constraints/messages/MessageUtil.java
+++ statix.solver/src/main/java/mb/statix/constraints/messages/MessageUtil.java
@@ -2,22 +2,28 @@ package mb.statix.constraints.messages;
 
 import mb.statix.solver.IConstraint;
 
+import javax.annotation.Nullable;
+
 public class MessageUtil {
+
+    public static IMessage findClosestMessage(IConstraint c) {
+        return findClosestMessage(c, MessageKind.ERROR);
+    }
 
     /**
      * Find closest message in the
      */
-    public static IMessage findClosestMessage(IConstraint c) {
-        IMessage message = null;
+    public static IMessage findClosestMessage(IConstraint c, MessageKind kind) {
+        @Nullable IMessage message = null;
         while(c != null) {
-            IMessage m;
+            @Nullable IMessage m;
             if((m = c.message().orElse(null)) != null && (message == null || message.kind().isWorseThan(m.kind()))) {
                 message = m;
             }
             c = c.cause().orElse(null);
         }
         if(message == null) {
-            message = new Message(MessageKind.ERROR);
+            message = new Message(kind);
         }
         return message;
     }
