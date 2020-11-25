6a7119510e9797e5a4ff8941d903d465a67bdff9
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2020-11-25 01:17:22 +0100

    Add timeout to result get.

--- statix.solver/src/main/java/mb/statix/concurrent/actors/futures/IFuture.java
+++ statix.solver/src/main/java/mb/statix/concurrent/actors/futures/IFuture.java
@@ -2,6 +2,8 @@ package mb.statix.concurrent.actors.futures;
 
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.metaborg.util.functions.CheckedAction1;
 import org.metaborg.util.functions.CheckedAction2;
@@ -14,15 +16,19 @@ public interface IFuture<T> {
 
     IFuture<Void> thenAccept(CheckedAction1<? super T, ? extends Throwable> handler);
 
-    <U> IFuture<U> thenCompose(CheckedFunction1<? super T, ? extends IFuture<? extends U>, ? extends Throwable> handler);
+    <U> IFuture<U>
+            thenCompose(CheckedFunction1<? super T, ? extends IFuture<? extends U>, ? extends Throwable> handler);
 
     <U> IFuture<U> handle(CheckedFunction2<? super T, Throwable, ? extends U, ? extends Throwable> handler);
 
-    <U> IFuture<U> compose(CheckedFunction2<? super T, Throwable, ? extends IFuture<? extends U>, ? extends Throwable> handler);
+    <U> IFuture<U> compose(
+            CheckedFunction2<? super T, Throwable, ? extends IFuture<? extends U>, ? extends Throwable> handler);
 
     IFuture<T> whenComplete(CheckedAction2<? super T, Throwable, ? extends Throwable> handler);
 
     T get() throws ExecutionException, InterruptedException;
+
+    T get(long timeout, TimeUnit unit) throws ExecutionException, InterruptedException, TimeoutException;
 
     T getNow() throws CompletionException, InterruptedException;
 
