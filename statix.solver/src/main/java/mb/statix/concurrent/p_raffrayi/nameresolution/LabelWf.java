08ca637252f83de20a200b92047779b88b46ec27
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2021-03-23 17:15:34 +0100

    Add static any() implementation.

--- statix.solver/src/main/java/mb/statix/concurrent/p_raffrayi/nameresolution/LabelWf.java
+++ statix.solver/src/main/java/mb/statix/concurrent/p_raffrayi/nameresolution/LabelWf.java
@@ -8,4 +8,18 @@ public interface LabelWf<L> {
 
     boolean accepting();
 
+    static <L> LabelWf<L> any() {
+        return new LabelWf<L>() {
+
+            @Override public boolean accepting() {
+                return true;
+            }
+
+            @Override public Optional<LabelWf<L>> step(L l) {
+                return Optional.of(this);
+            }
+
+        };
+    }
+
 }
