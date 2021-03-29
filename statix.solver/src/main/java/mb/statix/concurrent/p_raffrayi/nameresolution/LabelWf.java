c5538646822395de2f2b99be55e54e6ab8113241
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2021-03-29 13:31:17 +0200

    Make LabelWF.any() a singleton.

--- statix.solver/src/main/java/mb/statix/concurrent/p_raffrayi/nameresolution/LabelWf.java
+++ statix.solver/src/main/java/mb/statix/concurrent/p_raffrayi/nameresolution/LabelWf.java
@@ -9,17 +9,23 @@ public interface LabelWf<L> {
     boolean accepting();
 
     static <L> LabelWf<L> any() {
-        return new LabelWf<L>() {
-
-            @Override public boolean accepting() {
-                return true;
-            }
-
-            @Override public Optional<LabelWf<L>> step(L l) {
-                return Optional.of(this);
-            }
-
-        };
+        return ANY;
     }
+
+    static final LabelWf ANY = new LabelWf() {
+
+        @Override public boolean accepting() {
+            return true;
+        }
+
+        @Override public Optional<LabelWf> step(Object l) {
+            return Optional.of(this);
+        }
+
+        @Override public String toString() {
+            return ".*";
+        }
+
+    };
 
 }
