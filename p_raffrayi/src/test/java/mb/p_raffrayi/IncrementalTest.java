315271f46438ab33b09ab018e16e44cdd7bc90c9
Aron Zwaan <aronzwaan@gmail.com>
2021-05-07 17:42:03 +0200

    Add tests for mutual dependencies, where one of them is changed.

--- p_raffrayi/src/test/java/mb/p_raffrayi/IncrementalTest.java
+++ p_raffrayi/src/test/java/mb/p_raffrayi/IncrementalTest.java
@@ -213,7 +213,7 @@ public class IncrementalTest extends PRaffrayiTestBase {
                                 final Scope s1 = rootScopes.get(0);
                                 unit.initScope(s1, Arrays.asList(lbl), false);
                                 return unit.runIncremental(restarted -> {
-                                    final Scope d = unit.freshScope("d", Arrays.asList(lbl), true, false);
+                                    final Scope d = unit.freshScope("d", Arrays.asList(), true, false);
                                     unit.setDatum(d, d);
                                     unit.addEdge(s1, lbl, d);
                                     unit.closeEdge(s1, lbl);
@@ -227,6 +227,130 @@ public class IncrementalTest extends PRaffrayiTestBase {
                         return unit.runIncremental(restarted -> CompletableFuture.completedFuture(true))
                                 .thenCompose(res -> subResult.thenApply(sRes -> {
                                     return !res && !sRes.analysis() && sRes.failures().isEmpty();
+                                }));
+                    }
+
+                }, Set.Immutable.of(lbl), AInitialState.cached(parentResult));
+
+        final IUnitResult<Scope, IDatum, IDatum, Boolean> result = future.asJavaCompletion().get();
+        assertTrue(result.analysis());
+        assertTrue(result.failures().isEmpty());
+    }
+
+    @Test(timeout = 10000) public void testRelease_MutualDep_ParentChanged() throws InterruptedException, ExecutionException {
+        final Scope root = new Scope("/.", 0);
+        final IDatum lbl = new IDatum() {};
+        final Scope d = new Scope("/./sub", 1);
+
+        final ResolutionPath<Scope, IDatum, IDatum> path = new ScopePath<Scope, IDatum>(root).step(lbl, d).get().resolve(d);
+        final Env<Scope, IDatum, IDatum> env = Env.of(path);
+
+        final IUnitResult<Scope, IDatum, IDatum, Boolean> parentResult = UnitResult.<Scope, IDatum, IDatum, Boolean>builder()
+                .id("/.")
+                .scopeGraph(ScopeGraph.Immutable.<Scope, IDatum, IDatum>of().addEdge(root, lbl, d))
+                .addQueries(RecordedQuery.of(root, LabelWf.any(), DataWf.any(), LabelOrder.none(), DataLeq.any(), env))
+                .analysis(false)
+                .build();
+
+        final IUnitResult<Scope, IDatum, IDatum, Boolean> childResult = UnitResult.<Scope, IDatum, IDatum, Boolean>builder()
+                .id("/./sub")
+                .addRootScopes(root)
+                .scopeGraph(ScopeGraph.Immutable.<Scope, IDatum, IDatum>of().addEdge(root, lbl, d).setDatum(d, d))
+                .addQueries(RecordedQuery.of(root, LabelWf.any(), DataWf.any(), LabelOrder.none(), DataLeq.any(), env))
+                .analysis(false)
+                .build();
+
+        final IFuture<IUnitResult<Scope, IDatum, IDatum, Boolean>> future =
+                this.run(".", new ITypeChecker<Scope, IDatum, IDatum, Boolean>() {
+
+                    @Override public IFuture<Boolean> run(
+                            IIncrementalTypeCheckerContext<Scope, IDatum, IDatum, Boolean> unit, List<Scope> roots,
+                            IInitialState<Scope, IDatum, IDatum, Boolean> initialState) {
+                        final Scope s = unit.freshScope("s", Arrays.asList(), false, true);
+                        final IFuture<IUnitResult<Scope, IDatum, IDatum, Boolean>> subResult = unit.add("sub", new ITypeChecker<Scope, IDatum, IDatum, Boolean>() {
+
+                            @Override public IFuture<Boolean> run(
+                                    IIncrementalTypeCheckerContext<Scope, IDatum, IDatum, Boolean> unit,
+                                    List<Scope> rootScopes, IInitialState<Scope, IDatum, IDatum, Boolean> initialState) {
+                                final Scope s1 = rootScopes.get(0);
+                                unit.initScope(s1, Arrays.asList(lbl), false);
+                                return unit.runIncremental(restarted -> {
+                                    final Scope d = unit.freshScope("d", Arrays.asList(), true, false);
+                                    unit.setDatum(d, d);
+                                    unit.addEdge(s1, lbl, d);
+                                    unit.closeEdge(s1, lbl);
+
+                                    return unit.query(s1, LabelWf.any(), LabelOrder.none(), DataWf.any(), DataLeq.any()).thenApply(__ -> true);
+                                });
+                            }}, Arrays.asList(s), AInitialState.cached(childResult));
+
+                        unit.closeScope(s);
+
+                        return unit.runIncremental(restarted -> CompletableFuture.completedFuture(true))
+                                .thenCompose(res -> subResult.thenApply(sRes -> {
+                                    return res && sRes.analysis() && sRes.failures().isEmpty();
+                                }));
+                    }
+
+                }, Set.Immutable.of(lbl), AInitialState.changed(parentResult));
+
+        final IUnitResult<Scope, IDatum, IDatum, Boolean> result = future.asJavaCompletion().get();
+        assertTrue(result.analysis());
+        assertTrue(result.failures().isEmpty());
+    }
+
+    @Test(timeout = 10000) public void testRelease_MutualDep_ChildChanged() throws InterruptedException, ExecutionException {
+        final Scope root = new Scope("/.", 0);
+        final IDatum lbl = new IDatum() {};
+        final Scope d = new Scope("/./sub", 1);
+
+        final ResolutionPath<Scope, IDatum, IDatum> path = new ScopePath<Scope, IDatum>(root).step(lbl, d).get().resolve(d);
+        final Env<Scope, IDatum, IDatum> env = Env.of(path);
+
+        final IUnitResult<Scope, IDatum, IDatum, Boolean> parentResult = UnitResult.<Scope, IDatum, IDatum, Boolean>builder()
+                .id("/.")
+                .scopeGraph(ScopeGraph.Immutable.<Scope, IDatum, IDatum>of().addEdge(root, lbl, d))
+                .addQueries(RecordedQuery.of(root, LabelWf.any(), DataWf.any(), LabelOrder.none(), DataLeq.any(), env))
+                .analysis(false)
+                .build();
+
+        final IUnitResult<Scope, IDatum, IDatum, Boolean> childResult = UnitResult.<Scope, IDatum, IDatum, Boolean>builder()
+                .id("/./sub")
+                .addRootScopes(root)
+                .scopeGraph(ScopeGraph.Immutable.<Scope, IDatum, IDatum>of().addEdge(root, lbl, d).setDatum(d, d))
+                .addQueries(RecordedQuery.of(root, LabelWf.any(), DataWf.any(), LabelOrder.none(), DataLeq.any(), env))
+                .analysis(false)
+                .build();
+
+        final IFuture<IUnitResult<Scope, IDatum, IDatum, Boolean>> future =
+                this.run(".", new ITypeChecker<Scope, IDatum, IDatum, Boolean>() {
+
+                    @Override public IFuture<Boolean> run(
+                            IIncrementalTypeCheckerContext<Scope, IDatum, IDatum, Boolean> unit, List<Scope> roots,
+                            IInitialState<Scope, IDatum, IDatum, Boolean> initialState) {
+                        final Scope s = unit.freshScope("s", Arrays.asList(), false, true);
+                        final IFuture<IUnitResult<Scope, IDatum, IDatum, Boolean>> subResult = unit.add("sub", new ITypeChecker<Scope, IDatum, IDatum, Boolean>() {
+
+                            @Override public IFuture<Boolean> run(
+                                    IIncrementalTypeCheckerContext<Scope, IDatum, IDatum, Boolean> unit,
+                                    List<Scope> rootScopes, IInitialState<Scope, IDatum, IDatum, Boolean> initialState) {
+                                final Scope s1 = rootScopes.get(0);
+                                unit.initScope(s1, Arrays.asList(lbl), false);
+                                return unit.runIncremental(restarted -> {
+                                    final Scope d = unit.freshScope("d", Arrays.asList(), true, false);
+                                    unit.setDatum(d, d);
+                                    unit.addEdge(s1, lbl, d);
+                                    unit.closeEdge(s1, lbl);
+
+                                    return unit.query(s1, LabelWf.any(), LabelOrder.none(), DataWf.any(), DataLeq.any()).thenApply(__ -> true);
+                                });
+                            }}, Arrays.asList(s), AInitialState.changed(childResult));
+
+                        unit.closeScope(s);
+
+                        return unit.runIncremental(restarted -> CompletableFuture.completedFuture(true))
+                                .thenCompose(res -> subResult.thenApply(sRes -> {
+                                    return res && sRes.analysis() && sRes.failures().isEmpty();
                                 }));
                     }
 
