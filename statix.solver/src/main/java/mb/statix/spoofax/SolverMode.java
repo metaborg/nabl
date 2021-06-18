c49b05efa1e676d1e07d93bab7d65fd1170df144
Aron Zwaan <aronzwaan@gmail.com>
2021-06-18 11:15:08 +0200

    Add properties on SolverMode

--- statix.solver/src/main/java/mb/statix/spoofax/SolverMode.java
+++ statix.solver/src/main/java/mb/statix/spoofax/SolverMode.java
@@ -1,7 +1,22 @@
 package mb.statix.spoofax;
 
 public enum SolverMode {
-    TRADITIONAL, CONCURRENT, INCREMENTAL_DEADLOCK;
+    // @formatter:off
+    TRADITIONAL(false, false, false),
+    CONCURRENT(true, false, false),
+    INCREMENTAL_DEADLOCK(true, true, false),
+    INCREMENTAL_SCOPEGRAPH_DIFF(true, true, true);
+    // @formatter:on
 
     public static SolverMode DEFAULT = TRADITIONAL;
+
+    public final boolean concurrent;
+    public final boolean deadlock;
+    public final boolean sgDiff;
+
+    private SolverMode(boolean concurrent, boolean deadlock, boolean sgDiff) {
+        this.concurrent = concurrent;
+        this.deadlock = deadlock;
+        this.sgDiff = sgDiff;
+    }
 }
