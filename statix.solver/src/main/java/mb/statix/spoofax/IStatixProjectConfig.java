e323183936cd5ddb3a3e102982d678addb8fb1a8
Aron Zwaan <aronzwaan@gmail.com>
2021-06-18 17:48:17 +0200

    Properly order project config

--- statix.solver/src/main/java/mb/statix/spoofax/IStatixProjectConfig.java
+++ statix.solver/src/main/java/mb/statix/spoofax/IStatixProjectConfig.java
@@ -4,16 +4,17 @@ import java.util.Map;
 
 public interface IStatixProjectConfig {
 
+    public static int DEFAULT_MESSAGE_TRACE_LENGTH = 0;
+    public static int DEFAULT_MESSAGE_TERM_DEPTH = 3;
+
+    public static IStatixProjectConfig NULL = new StatixProjectConfig(null, null, null);
+
     SolverMode languageMode(String languageId, SolverMode defaultMode);
+
     Map<String, SolverMode> languageModes(Map<String, SolverMode> defaultModes);
 
     Integer messageTraceLength(Integer defaultValue);
 
     Integer messageTermDepth(Integer defaultValue);
-
-    public static int DEFAULT_MESSAGE_TRACE_LENGTH = 0;
-    public static int DEFAULT_MESSAGE_TERM_DEPTH = 3;
-
-    public static IStatixProjectConfig NULL = new StatixProjectConfig(null, null, null);
 
 }
