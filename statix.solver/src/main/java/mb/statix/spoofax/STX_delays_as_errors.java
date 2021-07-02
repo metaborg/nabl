2bd71b22e62f43e3b9ba05c1c73c8b6fc52478b4
Aron Zwaan <aronzwaan@gmail.com>
2021-07-02 14:58:56 +0200

    Recursively ignore delays on variables involved in failed constraints

--- statix.solver/src/main/java/mb/statix/spoofax/STX_delays_as_errors.java
+++ statix.solver/src/main/java/mb/statix/spoofax/STX_delays_as_errors.java
@@ -6,8 +6,10 @@ import static mb.nabl2.terms.matching.TermMatch.M;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 
+import org.metaborg.util.collection.CapsuleUtil;
 import org.metaborg.util.functions.Action1;
 import org.metaborg.util.functions.Function0;
 import org.spoofax.interpreter.core.IContext;
@@ -22,10 +24,12 @@ import mb.nabl2.terms.ITerm;
 import mb.nabl2.terms.ITermVar;
 import mb.nabl2.terms.substitution.IRenaming;
 import mb.nabl2.terms.substitution.ISubstitution.Immutable;
+import mb.nabl2.terms.unification.ud.IUniDisunifier;
 import mb.nabl2.util.TermFormatter;
 import mb.statix.constraints.messages.IMessage;
 import mb.statix.constraints.messages.MessageKind;
 import mb.statix.constraints.messages.MessageUtil;
+import mb.statix.solver.Delay;
 import mb.statix.solver.IConstraint;
 import mb.statix.solver.persistent.SolverResult;
 
@@ -43,25 +47,45 @@ public class STX_delays_as_errors extends StatixPrimitive {
 
         // Collect all free variables in 'real' errors
         // @formatter:off
-        final Set.Immutable<ITermVar> errorVars = result.messages().entrySet().stream()
+        final IUniDisunifier.Immutable unifier = result.state().unifier();
+        Set.Immutable<ITermVar> newErrorVars = result.messages().entrySet().stream()
             .filter(e -> e.getValue().kind().equals(MessageKind.ERROR))
             .map(Map.Entry::getKey)
             .flatMap(c -> c.freeVars().stream())
-            .flatMap(v -> result.state().unifier().findRecursive(v).getVars().stream())
+            .flatMap(v -> unifier.findRecursive(v).getVars().stream())
             .collect(CapsuleCollectors.toSet());
         // @formatter:on
+        Set.Immutable<ITermVar> allErrorVars = newErrorVars;
 
         // Collect all delays that involve variables that do not occur on free variables.
+        ImmutableMap<IConstraint, Delay> delays = result.delays();
+
+        while(!newErrorVars.isEmpty()) {
+            final Set.Transient<ITermVar> _newVars = CapsuleUtil.transientSet();
+            final ImmutableMap.Builder<IConstraint, Delay> retainedDelays = ImmutableMap.builder();
+
+            for(Entry<IConstraint, Delay> e : delays.entrySet()) {
+                if(e.getValue().vars().stream().anyMatch(newErrorVars::contains)) {
+                    for(ITermVar var : e.getKey().freeVars()) {
+                        _newVars.__insertAll(unifier.findRecursive(var).getVars().__removeAll(allErrorVars));
+                    }
+                } else {
+                    retainedDelays.put(e);
+                }
+            }
+
+            delays = retainedDelays.build();
+            newErrorVars = _newVars.freeze();
+            allErrorVars = allErrorVars.__insertAll(newErrorVars);
+        }
+
         final ImmutableMap.Builder<IConstraint, IMessage> messages = ImmutableMap.builder();
         messages.putAll(result.messages());
-        result.delays().entrySet().forEach(e -> {
-            if(e.getValue().vars().stream().noneMatch(errorVars::contains)) {
-                final IConstraint c = e.getKey();
-                messages.put(c, new Unsolved(MessageUtil.findClosestMessage(c)));
-            } else {
-                logger.debug("Ignoring delay {} because there is already an error on the delayed variables.", e);
-            }
+
+        delays.forEach((c, d) -> {
+            messages.put(c, new Unsolved(MessageUtil.findClosestMessage(c)));
         });
+
         final SolverResult newResult = result.withMessages(messages.build()).withDelays(ImmutableMap.of());
         return Optional.of(B.newBlob(newResult));
     }
