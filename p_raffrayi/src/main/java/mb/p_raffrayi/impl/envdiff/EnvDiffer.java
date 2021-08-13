b9cd4736c75dd8fd05b07f4ef45908ab78bf5a62
Aron Zwaan <aronzwaan@gmail.com>
2021-08-13 11:59:19 +0200

    Add logging to environment differ

--- p_raffrayi/src/main/java/mb/p_raffrayi/impl/envdiff/EnvDiffer.java
+++ p_raffrayi/src/main/java/mb/p_raffrayi/impl/envdiff/EnvDiffer.java
@@ -7,6 +7,8 @@ import org.metaborg.util.functions.Action4;
 import org.metaborg.util.future.AggregateFuture;
 import org.metaborg.util.future.CompletableFuture;
 import org.metaborg.util.future.IFuture;
+import org.metaborg.util.log.ILogger;
+import org.metaborg.util.log.LoggerUtils;
 import org.metaborg.util.unit.Unit;
 
 import io.usethesource.capsule.Set;
@@ -17,6 +19,8 @@ import mb.scopegraph.ecoop21.LabelWf;
 import mb.scopegraph.oopsla20.diff.Edge;
 
 public class EnvDiffer<S, L, D> implements IEnvDiffer<S, L, D> {
+
+    private static final ILogger logger = LoggerUtils.logger(EnvDiffer.class);
 
     private final IDifferOps<S, L, D> differOps;
     private final IScopeGraphDiffer<S, L, D> scopeGraphDiffer;
@@ -32,26 +36,32 @@ public class EnvDiffer<S, L, D> implements IEnvDiffer<S, L, D> {
 
     @Override public IFuture<IEnvDiff<S, L, D>> diff(S scope, Set.Immutable<S> seenScopes, LabelWf<L> labelWf,
             DataWf<S, L, D> dataWf) {
-        if(!differOps.ownScope(scope) ) {
+        logger.debug("Computing env diff for {} ~ {}.", scope, labelWf);
+        if(!differOps.ownScope(scope)) {
+            logger.debug("{} external", scope);
             return CompletableFuture.completedFuture(External.of(scope, labelWf, dataWf));
         }
 
         return scopeGraphDiffer.scopeDiff(scope).thenCompose(scopeDiff -> {
             return scopeDiff.<IFuture<IEnvDiff<S, L, D>>>match(match -> {
+                logger.debug("{} matched", scope);
                 final DiffTreeBuilder<S, L, D> treeBuilder = new DiffTreeBuilder<>(scope, match.currentScope());
 
                 // Process all added/removed edges
                 // @formatter:off
                 traverseApplicable(match.addedEdges(), labelWf, seenScopes, (label, target, newSeenScopes, newLabelWf) -> {
+                    logger.debug("{} -{}-> {} added", scope, label, target);
                     treeBuilder.addSubTree(label, target, AddedEdge.of(target, newLabelWf, dataWf));
                 });
                 traverseApplicable(match.removedEdges(), labelWf, seenScopes, (label, target, newSeenScopes, newLabelWf) -> {
+                    logger.debug("{} -{}-> {} removed", scope, label, target);
                     treeBuilder.addSubTree(label, target, RemovedEdge.of(target, newLabelWf, dataWf));
                 });
 
                 // Asynchronously collect all sub environment diffs
                 final ArrayList<IFuture<Unit>> subEnvFutures = new ArrayList<>();
                 traverseApplicable(match.matchedEdges(), labelWf, seenScopes, (label, target, newSeenScopes, newLabelWf) -> {
+                    logger.debug("{} -{}-> {} matched. Computing difftree step.", scope, label, target);
                     subEnvFutures.add(diff(target, newSeenScopes, newLabelWf, dataWf).thenApply(subDiff -> {
                         treeBuilder.addSubTree(label, target, subDiff);
                         return Unit.unit;
@@ -59,8 +69,16 @@ public class EnvDiffer<S, L, D> implements IEnvDiffer<S, L, D> {
                 });
                 // @formatter:on
 
-                return AggregateFuture.of(subEnvFutures).thenApply(__ -> treeBuilder.build());
-            }, () -> CompletableFuture.completedFuture(RemovedEdge.of(scope, labelWf, dataWf)));
+                return AggregateFuture.of(subEnvFutures).thenApply(__ -> {
+                    logger.debug("env diff for {} ~ {} complete.", scope, labelWf);
+                    final IEnvDiff<S, L, D> diffTree = treeBuilder.build();
+                    logger.trace("diff value: {}", diffTree);
+                    return diffTree;
+                });
+            }, () -> {
+                logger.debug("{} removed", scope);
+                return CompletableFuture.completedFuture(RemovedEdge.of(scope, labelWf, dataWf));
+            });
         });
     }
 
