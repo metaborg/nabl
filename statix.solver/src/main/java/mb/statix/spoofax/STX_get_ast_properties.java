e5baa2aa378cd5b8f9115624cb4ed42198fa27e8
Aron Zwaan <aronzwaan@gmail.com>
2022-05-18 18:53:49 +0200

    Add Menu options to inspect properties.

---
+++ statix.solver/src/main/java/mb/statix/spoofax/STX_get_ast_properties.java
@@ -0,0 +1,53 @@
+package mb.statix.spoofax;
+
+import static mb.nabl2.terms.build.TermBuild.B;
+import static mb.nabl2.terms.matching.TermMatch.M;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.metaborg.util.tuple.Tuple2;
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.InterpreterException;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+
+import mb.nabl2.terms.ITerm;
+import mb.nabl2.terms.stratego.TermIndex;
+import mb.statix.solver.ITermProperty;
+import mb.statix.solver.persistent.SolverResult;
+
+public class STX_get_ast_properties extends StatixPropertyPrimitive {
+
+    @Inject public STX_get_ast_properties() {
+        super(STX_get_ast_properties.class.getSimpleName(), 1);
+    }
+
+    @Override protected Optional<? extends ITerm> call(IContext env, ITerm term, List<ITerm> terms)
+            throws InterpreterException {
+        final SolverResult analysis = M.blobValue(SolverResult.class).match(terms.get(0))
+                .orElseThrow(() -> new InterpreterException("Expected solver result."));
+        final Optional<TermIndex> maybeIndex = TermIndex.get(term);
+        if(maybeIndex.isPresent()) {
+            final TermIndex index = maybeIndex.get();
+            final ImmutableList.Builder<ITerm> props = ImmutableList.builder();
+            for(Map.Entry<Tuple2<TermIndex, ITerm>, ITermProperty> prop : analysis.state().termProperties()
+                    .entrySet()) {
+                if(prop.getKey()._1().equals(index)) {
+                    final ITerm propName = prop.getKey()._2();
+                    final ITerm propValue = instantiateValue(prop.getValue(), analysis);
+                    final ITerm multiplicity = explicate(prop.getValue().multiplicity());
+
+                    props.add(B.newAppl(STX_PROP_OP, propName, propValue, multiplicity));
+                }
+            }
+
+            return Optional.of(B.newList(props.build()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+}
