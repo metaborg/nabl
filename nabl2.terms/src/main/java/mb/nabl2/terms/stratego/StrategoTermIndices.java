b0a2cf6e175852f5fb07de8289197cd2694c14d4
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-02-02 15:38:47 +0100

    Add documentation

--- nabl2.terms/src/main/java/mb/nabl2/terms/stratego/StrategoTermIndices.java
+++ nabl2.terms/src/main/java/mb/nabl2/terms/stratego/StrategoTermIndices.java
@@ -12,6 +12,9 @@ import org.spoofax.interpreter.terms.ITermFactory;
 import org.spoofax.terms.TermFactory;
 import org.spoofax.terms.util.TermUtils;
 
+/**
+ * Works with term indices.
+ */
 public final class StrategoTermIndices {
 
     // Prevent instantiation.
@@ -23,10 +26,28 @@ public final class StrategoTermIndices {
 
     // index
 
+    /**
+     * Adds term indices to the terms in the given tree,
+     * overwriting any existing term indices.
+     *
+     * @param term the AST to annotate with term indices
+     * @param resource the resource identifier, used for the term indices
+     * @param termFactory the Stratego term factory to use
+     * @return the AST with term indices
+     */
     public static IStrategoTerm index(IStrategoTerm term, String resource, ITermFactory termFactory) {
         return new Indexer(resource, termFactory, true, 1).index(term);
     }
 
+    /**
+     * Adds term indices to the terms in the given tree,
+     * keeping any existing term indices.
+     *
+     * @param term the AST to annotate with term indices
+     * @param resource the resource identifier, used for the new term indices
+     * @param termFactory the Stratego term factory to use
+     * @return the AST with term indices
+     */
     public static IStrategoTerm indexMore(IStrategoTerm term, String resource, ITermFactory termFactory) {
         int maxId = findMaxId(term);
         if (maxId >= 0) {
@@ -35,6 +56,13 @@ public final class StrategoTermIndices {
             return index(term, resource, termFactory);
         }
     }
+
+    /**
+     * Finds the maximum used term index in the given subtree.
+     *
+     * @param term the subtree to check
+     * @return the maximum term index; or -1 if none were found
+     */
     private static int findMaxId(final IStrategoTerm term) {
         int maxId = get(term).map(TermIndex::getId).orElse(-1);
         for(IStrategoTerm subterm : term.getSubterms()) {
@@ -42,6 +70,8 @@ public final class StrategoTermIndices {
         }
         return maxId;
     }
+
+    /** Adds term indices. */
     private static class Indexer {
 
         private final String resource;
@@ -49,6 +79,15 @@ public final class StrategoTermIndices {
 
         private int nextId;
         private boolean overwrite;
+
+        /**
+         * Initializes the {@code Indexer} class.
+         *
+         * @param resource the resource identifier
+         * @param termFactory the Stratego term factory to use
+         * @param overwrite whether to overwrite/discard any existing term indices
+         * @param initialId the term index to start with
+         */
         public Indexer(String resource, ITermFactory termFactory, boolean overwrite, int initialId) {
             this.resource = resource;
             this.termFactory = termFactory;
@@ -56,7 +95,8 @@ public final class StrategoTermIndices {
             this.nextId = initialId;
         }
 
-        private IStrategoTerm index(final IStrategoTerm term) {
+        public IStrategoTerm index(final IStrategoTerm term) {
+            // Create a copy that we can modify
             // @formatter:off
             IStrategoTerm result = StrategoTerms.match(term,
                 StrategoTerms.<IStrategoTerm>cases(
@@ -70,17 +110,22 @@ public final class StrategoTermIndices {
                     plhdr -> termFactory.annotateTerm(termFactory.makePlaceholder(plhdr.getTemplate()), plhdr.getAnnotations())
                 ));
             // @formatter:on
+
+            // Set the term index
             if(overwrite || !get(term).isPresent()) {
                 // Associate the term's origin, if any, with the new term index
                 final TermIndex index1 = TermIndex.of(resource, nextId++);
                 final TermIndex index = TermOrigin.get(term).map(o -> o.put(index1)).orElse(index1);
                 result = put(index, result, termFactory);
             }
+
+            // Copy the original term's attachments to the new term
             termFactory.copyAttachments(term, result);
             return result;
         }
 
         private IStrategoList index(final IStrategoList list) {
+            // Create a copy that we can modify
             IStrategoList result;
             if(list.isEmpty()) {
                 result = termFactory.makeList(TermFactory.EMPTY_TERM_ARRAY, list.getAnnotations());
@@ -98,6 +143,7 @@ public final class StrategoTermIndices {
 
             // Copy the original term's attachments to the new term
             termFactory.copyAttachments(list, result);
+
             return result;
         }
 
@@ -109,11 +155,18 @@ public final class StrategoTermIndices {
 
     // erase
 
+    /**
+     * Removes term indices from the terms in the given tree.
+     *
+     * @param term the AST to clear of term indices
+     * @param termFactory the Stratego term factory to use
+     * @return the AST without term indices
+     */
     public static IStrategoTerm erase(IStrategoTerm term, ITermFactory termFactory) {
         return new Eraser(termFactory).erase(term);
-
     }
 
+    /** Removes term indices. */
     private static class Eraser {
 
         private final ITermFactory termFactory;
@@ -163,6 +216,12 @@ public final class StrategoTermIndices {
 
     // indices of terms
 
+    /**
+     * Gets the term index of the given term, if any.
+     *
+     * @param term the term to check
+     * @return the term index of the term, or {@link Optional#empty()} if the term does not have a term index
+     */
     public static Optional<TermIndex> get(IStrategoTerm term) {
         for(IStrategoTerm anno : term.getAnnotations()) {
             Optional<TermIndex> index = match(anno);
@@ -173,12 +232,29 @@ public final class StrategoTermIndices {
         return Optional.empty();
     }
 
+    /**
+     * Puts a term index on the given term.
+     *
+     * @param index the term index to add
+     * @param term the term to copy
+     * @param factory the Stratego term factory to use
+     * @return the copied term, with the term index added
+     * @param <T> the type of term
+     */
     public static <T extends IStrategoTerm> T put(TermIndex index, T term, ITermFactory factory) {
         @SuppressWarnings({ "unchecked" }) T result = (T) factory.annotateTerm(term,
                 factory.makeListCons(build(index, factory), removeFromAnnoList(term.getAnnotations(), factory)));
         return result;
     }
 
+    /**
+     * Removes a term index from the given term, if the term index is present.
+     *
+     * @param term the term to copy
+     * @param factory the Stratego term factory to use
+     * @return the copied term, with the term index removed
+     * @param <T>
+     */
     public static <T extends IStrategoTerm> T remove(T term, ITermFactory factory) {
         @SuppressWarnings({ "unchecked" }) T result =
                 (T) factory.annotateTerm(term, removeFromAnnoList(term.getAnnotations(), factory));
@@ -187,6 +263,13 @@ public final class StrategoTermIndices {
 
     // index terms
 
+    /**
+     * Builds a term that represents a term index.
+     *
+     * @param index the term index to represent
+     * @param factory the Stratego term factory to use
+     * @return the built term (usually used as an annotation)
+     */
     public static IStrategoTerm build(TermIndex index, ITermFactory factory) {
         final IStrategoConstructor ctor = factory.makeConstructor(OP, ARITY);
         final IStrategoTerm indexTerm =
@@ -195,6 +278,11 @@ public final class StrategoTermIndices {
         return indexTerm;
     }
 
+    /**
+     * Tests whether the given term is a term index (usually used as an annotation).
+     * @param term the term to check
+     * @return the term index represented by the given term; or {@link Optional#empty()} if the term is not a term index
+     */
     public static Optional<TermIndex> match(IStrategoTerm term) {
         if(!TermUtils.isAppl(term, OP, ARITY)) {
             return Optional.empty();
