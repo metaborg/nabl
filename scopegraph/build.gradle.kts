4a58876bc61f8afc60e9404f114d63446b0d1e35
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-02-16 11:18:09 +0100

    Update dependencies

--- scopegraph/build.gradle.kts
+++ scopegraph/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
   api("com.google.guava:guava")
   // Required for Guava >= 27.0:
-  // api("com.google.guava:failureaccess")
+  api("com.google.guava:failureaccess")
   api("io.usethesource:capsule")
   compileOnly("com.google.code.findbugs:jsr305")
 
