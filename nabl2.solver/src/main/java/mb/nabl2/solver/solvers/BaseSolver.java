74ea066c0ad9c8cda57fd937010b9e9870587cd2
Aron Zwaan <aronzwaan@gmail.com>
2023-07-18 15:29:50 +0200

    Add programming reproduction of tuple arity test

--- nabl2.solver/src/main/java/mb/nabl2/solver/solvers/BaseSolver.java
+++ nabl2.solver/src/main/java/mb/nabl2/solver/solvers/BaseSolver.java
@@ -41,102 +41,109 @@ import mb.scopegraph.pepm16.terms.Scope;
 
 public class BaseSolver {
 
-    protected final NaBL2DebugConfig nabl2Debug;
-    protected final CallExternal callExternal;
+	protected final NaBL2DebugConfig nabl2Debug;
+	protected final CallExternal callExternal;
 
-    public BaseSolver(NaBL2DebugConfig nabl2Debug, CallExternal callExternal) {
-        this.nabl2Debug = nabl2Debug;
-        this.callExternal = callExternal;
-    }
+	public BaseSolver(NaBL2DebugConfig nabl2Debug, CallExternal callExternal) {
+		this.nabl2Debug = nabl2Debug;
+		this.callExternal = callExternal;
+	}
 
-    public GraphSolution solveGraph(BaseSolution initial, Function1<String, String> fresh, ICancel cancel,
-            IProgress progress) throws SolverException, InterruptedException {
+	public GraphSolution solveGraph(BaseSolution initial, Function1<String, String> fresh, ICancel cancel,
+			IProgress progress) throws SolverException, InterruptedException {
 
-        // shared
-        final Ref<IUnifier.Immutable> unifier = new Ref<>(initial.unifier());
-        final IEsopScopeGraph.Transient<Scope, Label, Occurrence, ITerm> scopeGraph = EsopScopeGraph.Transient.of();
-        final ScopeGraphReducer scopeGraphReducer = new ScopeGraphReducer(scopeGraph, unifier);
+		// shared
+		final Ref<IUnifier.Immutable> unifier = new Ref<>(initial.unifier());
+		final IEsopScopeGraph.Transient<Scope, Label, Occurrence, ITerm> scopeGraph = EsopScopeGraph.Transient.of();
+		final ScopeGraphReducer scopeGraphReducer = new ScopeGraphReducer(scopeGraph, unifier);
 
-        // solver components
-        final SolverCore core = new SolverCore(initial.config(), unifier, fresh, callExternal, cancel, progress);
-        final AstComponent astSolver = new AstComponent(core, Properties.Transient.of());
-        final BaseComponent baseSolver = new BaseComponent(core);
-        final EqualityComponent equalitySolver = new EqualityComponent(core, unifier);
-        final ScopeGraphComponent scopeGraphSolver = new ScopeGraphComponent(core, scopeGraph);
-        final ExternalRelationComponent relationSolver = new ExternalRelationComponent(core);
+		// solver components
+		final SolverCore core = new SolverCore(initial.config(), unifier, fresh, callExternal, cancel, progress);
+		final AstComponent astSolver = new AstComponent(core, Properties.Transient.of());
+		final BaseComponent baseSolver = new BaseComponent(core);
+		final EqualityComponent equalitySolver = new EqualityComponent(core, unifier);
+		final ScopeGraphComponent scopeGraphSolver = new ScopeGraphComponent(core, scopeGraph);
+		final ExternalRelationComponent relationSolver = new ExternalRelationComponent(core);
 
-        try {
-            // @formatter:off
-            ISolver component = c -> c.matchOrThrow(IConstraint.CheckedCases.<SolveResult, DelayException>builder()
-                .onAst(astSolver::solve)
-                .onBase(baseSolver::solve)
-                .onEquality(equalitySolver::solve)
-                .onScopeGraph(scopeGraphSolver::solve)
-                .onRelation(relationSolver::solve)
-                .otherwise(ISolver.defer())
-            );
-            // @formatter:on
+		try {
+			// @formatter:off
+			ISolver component = c -> c.matchOrThrow(IConstraint.CheckedCases.<SolveResult, DelayException>builder()
+					.onAst(astSolver::solve).onBase(baseSolver::solve).onEquality(equalitySolver::solve)
+					.onScopeGraph(scopeGraphSolver::solve).onRelation(relationSolver::solve)
+					.otherwise(ISolver.defer()));
+			// @formatter:on
 
-            final FixedPointSolver solver = new FixedPointSolver(cancel, progress, component);
+			final FixedPointSolver solver = new FixedPointSolver(cancel, progress, component);
 
-            solver.step().subscribe(r -> {
-                final Set.Immutable<ITermVar> vars = r.result.unifierDiff().domainSet();
-                if(!vars.isEmpty()) {
-                    try {
-                        final List<CriticalEdge> criticalEdges = scopeGraphReducer.update(vars);
-                        r.resolveCriticalEdges(criticalEdges);
-                    } catch(InterruptedException ex) {
-                        // ignore here
-                    }
-                }
-            });
+			solver.step().subscribe(r -> {
+				final Set.Immutable<ITermVar> vars = r.result.unifierDiff().domainSet();
+				if (!vars.isEmpty()) {
+					try {
+						final List<CriticalEdge> criticalEdges = scopeGraphReducer.update(vars);
+						r.resolveCriticalEdges(criticalEdges);
+					} catch (InterruptedException ex) {
+						// ignore here
+					}
+				}
+			});
 
-            scopeGraphReducer.updateAll();
-            final SolveResult solveResult = solver.solve(initial.constraints(), unifier);
+			scopeGraphReducer.updateAll();
+			final SolveResult solveResult = solver.solve(initial.constraints(), unifier);
 
-            return GraphSolution.of(initial.config(), astSolver.finish(), scopeGraphSolver.finish(),
-                    equalitySolver.finish(), solveResult.messages(), solveResult.constraints());
-        } catch(RuntimeException ex) {
-            throw new SolverException("Internal solver error.", ex);
-        }
+			return GraphSolution.of(initial.config(), astSolver.finish(), scopeGraphSolver.finish(),
+					equalitySolver.finish(), solveResult.messages(), solveResult.constraints());
+		} catch (RuntimeException ex) {
+			throw new SolverException("Internal solver error.", ex);
+		}
 
-    }
+	}
 
-    protected boolean seed(SeedResult result, IMessages.Transient messages, java.util.Set<IConstraint> constraints) {
-        boolean change = false;
-        change |= messages.addAll(result.messages());
-        change |= constraints.addAll(result.constraints());
-        return change;
-    }
+	protected boolean seed(SeedResult result, IMessages.Transient messages, java.util.Set<IConstraint> constraints) {
+		boolean change = false;
+		change |= messages.addAll(result.messages());
+		change |= constraints.addAll(result.constraints());
+		return change;
+	}
 
-    @Value.Immutable
-    @Serial.Version(value = 42L)
-    public static abstract class ABaseSolution {
+	@Value.Immutable
+	@Serial.Version(value = 42L)
+	public static abstract class ABaseSolution {
 
-        @Value.Parameter public abstract SolverConfig config();
+		@Value.Parameter
+		public abstract SolverConfig config();
 
-        @Value.Parameter public abstract Set.Immutable<IConstraint> constraints();
+		@Value.Parameter
+//		public abstract Set.Immutable<IConstraint> constraints();
+		public abstract List<IConstraint> constraints();
 
-        @Value.Parameter public abstract IUnifier.Immutable unifier();
+		@Value.Parameter
+		public abstract IUnifier.Immutable unifier();
 
-    }
+	}
 
-    @Value.Immutable
-    @Serial.Version(value = 42L)
-    public static abstract class AGraphSolution {
+	@Value.Immutable
+	@Serial.Version(value = 42L)
+	public static abstract class AGraphSolution {
 
-        @Value.Parameter public abstract SolverConfig config();
+		@Value.Parameter
+		public abstract SolverConfig config();
 
-        @Value.Parameter public abstract IProperties.Immutable<TermIndex, ITerm, ITerm> astProperties();
+		@Value.Parameter
+		public abstract IProperties.Immutable<TermIndex, ITerm, ITerm> astProperties();
 
-        @Value.Parameter public abstract IEsopScopeGraph.Immutable<Scope, Label, Occurrence, ITerm> scopeGraph();
+		@Value.Parameter
+		public abstract IEsopScopeGraph.Immutable<Scope, Label, Occurrence, ITerm> scopeGraph();
 
-        @Value.Parameter public abstract IUnifier.Immutable unifier();
+		@Value.Parameter
+		public abstract IUnifier.Immutable unifier();
 
-        @Value.Parameter public abstract IMessages.Immutable messages();
+		@Value.Parameter
+		public abstract IMessages.Immutable messages();
 
-        @Value.Parameter public abstract Set.Immutable<IConstraint> constraints();
+		@Value.Parameter
+		public abstract List<IConstraint> constraints();
+//        @Value.Parameter public abstract Set.Immutable<IConstraint> constraints();
 
-    }
+	}
 
 }
---
+++ nabl2.solver/src/test/java/mb/nabl2/regression/TupleArityTest.java
@@ -0,0 +1,119 @@
+package mb.nabl2.regression;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.metaborg.util.collection.CapsuleUtil;
+import org.metaborg.util.task.NullCancel;
+import org.metaborg.util.task.NullProgress;
+import org.spoofax.interpreter.core.InterpreterException;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.interpreter.terms.ITermFactory;
+import org.spoofax.terms.TermFactory;
+import org.spoofax.terms.io.TAFTermReader;
+
+import mb.nabl2.config.NaBL2DebugConfig;
+import mb.nabl2.constraints.Constraints;
+import mb.nabl2.constraints.IConstraint;
+import mb.nabl2.constraints.base.IBaseConstraint;
+import mb.nabl2.solver.Fresh;
+import mb.nabl2.solver.ISolution;
+import mb.nabl2.solver.SolverConfig;
+import mb.nabl2.solver.exceptions.SolverException;
+import mb.nabl2.solver.solvers.BaseSolution;
+import mb.nabl2.solver.solvers.CallExternal;
+import mb.nabl2.solver.solvers.GraphSolution;
+import mb.nabl2.solver.solvers.SingleFileSolver;
+import mb.nabl2.stratego.ConstraintTerms;
+import mb.nabl2.terms.ITerm;
+import mb.nabl2.terms.stratego.StrategoTerms;
+import mb.nabl2.terms.unification.Unifiers;
+import mb.scopegraph.pepm16.terms.ResolutionParameters;
+
+@RunWith(Parameterized.class)
+public class TupleArityTest {
+
+	private static final int COUNT = 100000;
+	private static final Random rnd = new Random(34856);
+
+
+	private static final ITermFactory TF = new TermFactory();
+	private static final StrategoTerms ST = new StrategoTerms(TF);
+	private static final TAFTermReader reader = new TAFTermReader(TF);
+
+
+    @Parameterized.Parameters
+    public static Object[][] data() throws IOException, InterpreterException {
+		final IStrategoTerm constraintSTerm = reader
+				.read(TupleArityTest.class.getResourceAsStream("test.constraints.aterm"));
+		final ITerm constraintTerm = ConstraintTerms.specialize(ST.fromStratego(constraintSTerm.getSubterm(0)));
+		final IConstraint constraint = Constraints.matchConstraintOrList().match(constraintTerm)
+				.orElseThrow(() -> new InterpreterException("Current term is not a constraint."));
+		final List<IConstraint> constraints = disjoin(constraint);
+
+		final Object[][] result = new Object[COUNT][1];
+		for(int i = 0; i < COUNT; i++) {
+			Collections.shuffle(constraints, rnd);
+			result[i][0] = new ArrayList<>(constraints);
+		}
+		return result;
+    }
+
+    public TupleArityTest(List<IConstraint> permutation) {
+		this.permutation = permutation;
+    }
+
+	private final List<IConstraint> permutation;
+
+	@Test
+	public void testTupleArityConstraintSet()
+			throws IOException, InterpreterException, SolverException, InterruptedException {
+
+		final SingleFileSolver solver = new SingleFileSolver(NaBL2DebugConfig.NONE, CallExternal.never());
+		final Fresh.Transient fresh = Fresh.Transient.of();
+
+		final SolverConfig config = SolverConfig.of(ResolutionParameters.getDefault(), Collections.emptyMap(),
+				Collections.emptyMap());
+
+		final GraphSolution gs = solver.solveGraph(
+				BaseSolution.of(config, permutation, Unifiers.Immutable.of()), fresh::fresh,
+				new NullCancel(), new NullProgress());
+
+		final ISolution solution = solver.solve(gs, fresh::fresh, new NullCancel(), new NullProgress());
+
+        Assert.assertFalse("Missing errors", solution.messagesAndUnsolvedErrors().getErrors().isEmpty());
+	}
+
+	public static List<IConstraint> disjoin(IConstraint constraint) {
+		final ArrayList<IConstraint> constraints = new ArrayList<>();
+		disjoin(constraint, constraints);
+		return constraints;
+	}
+
+	private static boolean disjoin(IConstraint constraint, ArrayList<IConstraint> constraints) {
+		return constraint.match(IConstraint.Cases.of(
+				c -> constraints.add(c),
+				c -> c.match(IBaseConstraint.Cases.of(
+						bc -> constraints.add(bc),
+						bc -> constraints.add(bc),
+						bc -> constraints.add(bc.getLeft()) && constraints.add(bc.getRight()),
+						bc -> constraints.add(bc),
+						bc -> constraints.add(bc))),
+				c -> constraints.add(c),
+				c -> constraints.add(c),
+				c -> constraints.add(c),
+				c -> constraints.add(c),
+				c -> constraints.add(c),
+				c -> constraints.add(c)
+		));
+
+	}
+
+}
