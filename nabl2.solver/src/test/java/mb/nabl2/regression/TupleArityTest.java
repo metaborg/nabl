c37546b234b74a48aeadd29dfcb9479f242fa80e
Aron Zwaan <aronzwaan@gmail.com>
2023-07-18 15:50:14 +0200

    Fix few bugs

--- nabl2.solver/src/test/java/mb/nabl2/regression/TupleArityTest.java
+++ nabl2.solver/src/test/java/mb/nabl2/regression/TupleArityTest.java
@@ -10,7 +10,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.metaborg.util.collection.CapsuleUtil;
 import org.metaborg.util.task.NullCancel;
 import org.metaborg.util.task.NullProgress;
 import org.spoofax.interpreter.core.InterpreterException;
@@ -40,70 +39,68 @@ import mb.scopegraph.pepm16.terms.ResolutionParameters;
 @RunWith(Parameterized.class)
 public class TupleArityTest {
 
-	private static final int COUNT = 100000;
-	private static final Random rnd = new Random(34856);
+    private static final int COUNT = 100000;
+    private static final Random rnd = new Random(34856);
 
 
-	private static final ITermFactory TF = new TermFactory();
-	private static final StrategoTerms ST = new StrategoTerms(TF);
-	private static final TAFTermReader reader = new TAFTermReader(TF);
+    private static final ITermFactory TF = new TermFactory();
+    private static final StrategoTerms ST = new StrategoTerms(TF);
+    private static final TAFTermReader reader = new TAFTermReader(TF);
 
 
-    @Parameterized.Parameters
-    public static Object[][] data() throws IOException, InterpreterException {
-		final IStrategoTerm constraintSTerm = reader
-				.read(TupleArityTest.class.getResourceAsStream("test.constraints.aterm"));
-		final ITerm constraintTerm = ConstraintTerms.specialize(ST.fromStratego(constraintSTerm.getSubterm(0)));
-		final IConstraint constraint = Constraints.matchConstraintOrList().match(constraintTerm)
-				.orElseThrow(() -> new InterpreterException("Current term is not a constraint."));
-		final List<IConstraint> constraints = disjoin(constraint);
+    @Parameterized.Parameters public static Object[][] data() throws IOException, InterpreterException {
+        final IStrategoTerm constraintSTerm =
+            reader.read(TupleArityTest.class.getResourceAsStream("test.constraints.aterm"));
+        final ITerm constraintTerm = ConstraintTerms.specialize(ST.fromStratego(constraintSTerm.getSubterm(0)));
+        final IConstraint constraint = Constraints.matchConstraintOrList().match(constraintTerm)
+            .orElseThrow(() -> new InterpreterException("Current term is not a constraint."));
+        final List<IConstraint> constraints = disjoin(constraint);
 
-		final Object[][] result = new Object[COUNT][1];
-		for(int i = 0; i < COUNT; i++) {
-			Collections.shuffle(constraints, rnd);
-			result[i][0] = new ArrayList<>(constraints);
-		}
-		return result;
+        final Object[][] result = new Object[COUNT][1];
+        for(int i = 0; i < COUNT; i++) {
+            Collections.shuffle(constraints, rnd);
+            result[i][0] = new ArrayList<>(constraints);
+        }
+        return result;
     }
 
     public TupleArityTest(List<IConstraint> permutation) {
-		this.permutation = permutation;
+        this.permutation = permutation;
     }
 
-	private final List<IConstraint> permutation;
+    private final List<IConstraint> permutation;
 
-	@Test
-	public void testTupleArityConstraintSet()
-			throws IOException, InterpreterException, SolverException, InterruptedException {
+    @Test public void testTupleArityConstraintSet()
+        throws IOException, InterpreterException, SolverException, InterruptedException {
 
-		final SingleFileSolver solver = new SingleFileSolver(NaBL2DebugConfig.NONE, CallExternal.never());
-		final Fresh.Transient fresh = Fresh.Transient.of();
+        final SingleFileSolver solver = new SingleFileSolver(NaBL2DebugConfig.NONE, CallExternal.never());
+        final Fresh.Transient fresh = Fresh.Transient.of();
 
-		final SolverConfig config = SolverConfig.of(ResolutionParameters.getDefault(), Collections.emptyMap(),
-				Collections.emptyMap());
+        final SolverConfig config =
+            SolverConfig.of(ResolutionParameters.getDefault(), Collections.emptyMap(), Collections.emptyMap());
 
-		final GraphSolution gs = solver.solveGraph(
-				BaseSolution.of(config, permutation, Unifiers.Immutable.of()), fresh::fresh,
-				new NullCancel(), new NullProgress());
+        final GraphSolution gs = solver.solveGraph(BaseSolution.of(config, permutation, Unifiers.Immutable.of()),
+            fresh::fresh, new NullCancel(), new NullProgress());
 
-		final ISolution solution = solver.solve(gs, fresh::fresh, new NullCancel(), new NullProgress());
+        final ISolution solution = solver.solve(gs, fresh::fresh, new NullCancel(), new NullProgress());
 
         Assert.assertFalse("Missing errors", solution.messagesAndUnsolvedErrors().getErrors().isEmpty());
-	}
+    }
 
-	public static List<IConstraint> disjoin(IConstraint constraint) {
-		final ArrayList<IConstraint> constraints = new ArrayList<>();
-		disjoin(constraint, constraints);
-		return constraints;
-	}
+    public static List<IConstraint> disjoin(IConstraint constraint) {
+        final ArrayList<IConstraint> constraints = new ArrayList<>();
+        disjoin(constraint, constraints);
+        return constraints;
+    }
 
-	private static boolean disjoin(IConstraint constraint, ArrayList<IConstraint> constraints) {
+    private static boolean disjoin(IConstraint constraint, ArrayList<IConstraint> constraints) {
+        // @formatter:off
 		return constraint.match(IConstraint.Cases.of(
 				c -> constraints.add(c),
 				c -> c.match(IBaseConstraint.Cases.of(
 						bc -> constraints.add(bc),
 						bc -> constraints.add(bc),
-						bc -> constraints.add(bc.getLeft()) && constraints.add(bc.getRight()),
+						bc -> disjoin(bc.getLeft(), constraints) && disjoin(bc.getRight(), constraints),
 						bc -> constraints.add(bc),
 						bc -> constraints.add(bc))),
 				c -> constraints.add(c),
@@ -113,7 +110,7 @@ public class TupleArityTest {
 				c -> constraints.add(c),
 				c -> constraints.add(c)
 		));
-
-	}
+		// @formatter:on
+    }
 
 }
