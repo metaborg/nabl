bbfba4051a217158fd5bbbfdab97c0b41c61921c
Aron Zwaan <aronzwaan@gmail.com>
2023-07-24 15:34:05 +0200

    Make Java 11+ type parameter inference happy :)

--- scopegraph/src/main/java/mb/scopegraph/oopsla20/ScopeGraphUtil.java
+++ scopegraph/src/main/java/mb/scopegraph/oopsla20/ScopeGraphUtil.java
@@ -22,7 +22,10 @@ public final class ScopeGraphUtil {
         final Map<S, java.util.Set<Map.Entry<L, Iterable<S>>>> groupedScopes = sgEdges.entrySet().stream().collect(
             Collectors.groupingBy(
                 t -> t.getKey().getKey(), Collectors.mapping(
-                    t -> new AbstractMap.SimpleImmutableEntry<>(t.getKey().getValue(), t.getValue()), Collectors.toSet()
+                    t -> {
+                        final Map.Entry<L,Iterable<S>> result = new AbstractMap.SimpleImmutableEntry<>(t.getKey().getValue(), t.getValue());
+                        return result;
+                    }, Collectors.toSet()
                 )
             )
         );
