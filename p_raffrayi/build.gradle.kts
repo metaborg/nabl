66fdc998937bf9ed119027a9e0da047c7315cf43
Aron Zwaan <aronzwaan@gmail.com>
2023-07-24 14:29:05 +0200

    Optionally enable kapt

--- p_raffrayi/build.gradle.kts
+++ p_raffrayi/build.gradle.kts
@@ -4,12 +4,18 @@ plugins {
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
+// Used for refsyn: disabled by default
+fun kaptEnabled() = extra.has("kaptEnabled") && extra["kaptEnabled"] as Boolean
 val spoofax2Version: String by ext
 dependencies {
   api(platform("org.metaborg:parent:$spoofax2Version"))
   testImplementation(platform("org.metaborg:parent:$spoofax2Version"))
   annotationProcessor(platform("org.metaborg:parent:$spoofax2Version"))
   testAnnotationProcessor(platform("org.metaborg:parent:$spoofax2Version"))
+  if(kaptEnabled()) {
+    kapt(platform("org.metaborg:parent:$version"))
+    kaptTest(platform("org.metaborg:parent:$version"))
+  }
 
   // !! Update dependencies in pom.xml as well
 
@@ -22,6 +28,10 @@ dependencies {
   // Annotation processing
   annotationProcessor("org.immutables:value")
   annotationProcessor("org.immutables:serial")
+  if(kaptEnabled()) {
+    kapt("org.immutables:value")
+    kapt("org.immutables:serial")
+  }
   compileOnly("org.immutables:value")
   compileOnly("org.immutables:serial")
   compileOnly("javax.annotation:javax.annotation-api")
@@ -35,6 +45,10 @@ dependencies {
   // Test Annotation processing
   testAnnotationProcessor("org.immutables:value")
   testAnnotationProcessor("org.immutables:serial")
+  if(kaptEnabled()) {
+    kaptTest("org.immutables:value")
+    kaptTest("org.immutables:serial")
+  }
   testCompileOnly("org.immutables:value")
   testCompileOnly("org.immutables:serial")
   testCompileOnly("javax.annotation:javax.annotation-api")
--- statix.solver/build.gradle.kts
+++ statix.solver/build.gradle.kts
@@ -1,15 +1,21 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.gradle.config.junit-testing")
+  id("org.metaborg.gradle.config.java-library") version("0.4.7")
+  id("org.metaborg.gradle.config.junit-testing") version("0.4.7")
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
+// Used for refsyn: disabled by default
+fun kaptEnabled() = extra.has("kaptEnabled") && extra["kaptEnabled"] as Boolean
 val spoofax2Version: String by ext
 dependencies {
   api(platform("org.metaborg:parent:$spoofax2Version"))
   testImplementation(platform("org.metaborg:parent:$spoofax2Version"))
   annotationProcessor(platform("org.metaborg:parent:$spoofax2Version"))
   testAnnotationProcessor(platform("org.metaborg:parent:$spoofax2Version"))
+  if(kaptEnabled()) {
+    kapt(platform("org.metaborg:parent:$version"))
+    kaptTest(platform("org.metaborg:parent:$version"))
+  }
 
   // !! Update dependencies in pom.xml as well
 
@@ -26,9 +32,14 @@ dependencies {
   // Annotation processing
   annotationProcessor("org.immutables:value")
   annotationProcessor("org.immutables:serial")
+  if(kaptEnabled()) {
+    kapt("org.immutables:value")
+    kapt("org.immutables:serial")
+  }
   compileOnly("org.immutables:value")
   compileOnly("org.immutables:serial")
   compileOnly("javax.annotation:javax.annotation-api")
+  compileOnly("javax.inject:javax.inject:1")
 
   // Tests
   testImplementation("junit:junit")
@@ -39,6 +50,10 @@ dependencies {
   // Test Annotation processing
   testAnnotationProcessor("org.immutables:value")
   testAnnotationProcessor("org.immutables:serial")
+  if(kaptEnabled()) {
+    kaptTest("org.immutables:value")
+    kaptTest("org.immutables:serial")
+  }
   testCompileOnly("org.immutables:value")
   testCompileOnly("org.immutables:serial")
   testCompileOnly("javax.annotation:javax.annotation-api")
