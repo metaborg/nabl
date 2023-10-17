628d94c9a274e1b73ae6490822b8f282d277fa2b
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-10-17 13:38:27 +0200

    Add SolverTracer overloads

--- statix.solver/src/main/java/mb/statix/solver/tracer/SolverTracer.java
+++ statix.solver/src/main/java/mb/statix/solver/tracer/SolverTracer.java
@@ -1,15 +1,23 @@
 package mb.statix.solver.tracer;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Optional;
 
+import io.usethesource.capsule.Map;
+import io.usethesource.capsule.Set;
+import mb.nabl2.terms.ITermVar;
 import mb.statix.concurrent.StatixSolver;
+import mb.statix.solver.Delay;
 import mb.statix.solver.IConstraint;
 import mb.statix.solver.IState;
 import mb.statix.solver.IState.Immutable;
+import mb.statix.solver.completeness.ICompleteness;
 import mb.statix.solver.persistent.SolverResult;
 import mb.statix.solver.persistent.step.IStep;
 import mb.statix.solver.persistent.step.StepResult;
+
+import javax.annotation.Nullable;
 
 /**
  * Tracers allow to observe the behavior of a Statix solver externally. When an {@link SolverTracer} instance is passed
@@ -35,6 +43,7 @@ public abstract class SolverTracer<R extends SolverTracer.IResult<R>> {
 
     public void onTrySolveConstraint(IConstraint constraint, IState.Immutable state) {
 
+        // Override this implementation to handle this event
     }
 
     public Optional<StepResult> onStep(IStep step, IState.Immutable oldState) {
@@ -42,20 +51,59 @@ public abstract class SolverTracer<R extends SolverTracer.IResult<R>> {
                 (ns, up, nc, nce, ne) -> onConstraintSolved(step.constraint(), ns),
                 ex -> onConstraintFailed(step.constraint(), oldState),
                 dl -> onConstraintDelayed(step.constraint(), oldState)
+        return step.result().match(
+            (ns, up, nc, nce, ne) -> onConstraintSolved(step.constraint(), ns, up, nc, nce, ne),
+            ex -> onConstraintFailed(step.constraint(), oldState, ex),
+            dl -> onConstraintDelayed(step.constraint(), oldState, dl)
         );
+    }
+    public Optional<StepResult> onConstraintSolved(
+            IConstraint constraint,
+            IState.Immutable newState,
+            Set.Immutable<ITermVar> updatedVars,
+            Collection<IConstraint> newConstraints,
+            ICompleteness.Immutable newCriticalEdges,
+            Map.Immutable<ITermVar, ITermVar> newExistentials
+    ) {
+        // Override this implementation to handle this event
+        onConstraintSolved(constraint, newState);
         return Optional.empty();
     }
 
     public void onConstraintSolved(IConstraint constraint, IState.Immutable state) {
+    public void onConstraintSolved(IConstraint constraint, IState.Immutable newState) {
+        // Override this implementation to handle this event
+    }
 
+    public Optional<StepResult> onConstraintFailed(
+            IConstraint constraint,
+            IState.Immutable newState,
+            @Nullable Throwable exception
+    ) {
+        // Override this implementation to handle this event
+        onConstraintFailed(constraint, newState);
+        return Optional.empty();
     }
 
     public void onConstraintDelayed(IConstraint constraint, IState.Immutable state) {
+    public void onConstraintFailed(IConstraint constraint, IState.Immutable state) {
+        // Override this implementation to handle this event
+    }
 
+    public Optional<StepResult> onConstraintDelayed(
+            IConstraint constraint,
+            IState.Immutable newState,
+            Delay delay
+    ) {
+        // Override this implementation to handle this event
+        onConstraintDelayed(constraint, newState);
+        return Optional.empty();
     }
 
     public void onConstraintFailed(IConstraint constraint, IState.Immutable state) {
 
+    public void onConstraintDelayed(IConstraint constraint, IState.Immutable state) {
+        // Override this implementation to handle this event
     }
 
     public interface IResult<SELF extends IResult<SELF>> extends Serializable {
