a3310dd2c0ec39d5d7578d2b00166a94c58b91ca
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2023-12-18 16:38:01 +0100

    Add FIXMEs

--- nabl2.terms/build.gradle.kts
+++ nabl2.terms/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 //  id("org.metaborg.gradle.config.junit-testing")
 }
 
+// FIXME: Move this to a common spot
 repositories {
     mavenCentral()
     maven("https://nexus.usethesource.io/content/repositories/releases/")
@@ -11,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    // FIXME: Move these platform definitions to a common spot
     api(platform(libs.spoofax3.bom))
     testImplementation(platform(libs.spoofax3.bom))
     annotationProcessor(platform(libs.spoofax3.bom))
@@ -40,6 +42,7 @@ dependencies {
     testCompileOnly(libs.immutables.serial)
 }
 
+// FIXME: If this is necessary, use testFixtures instead
 //// Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
 //val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
 //  from("$projectDir/src/test/resources")
