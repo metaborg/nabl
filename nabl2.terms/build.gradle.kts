fa53cad51007fc815e8af3d87355edcb472cf45c
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2023-12-18 16:36:22 +0100

    Fix nabl2.terms project

--- nabl2.terms/build.gradle.kts
+++ nabl2.terms/build.gradle.kts
@@ -1,47 +1,48 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.gradle.config.junit-testing")
+    `java-library`
+//  id("org.metaborg.gradle.config.java-library")
+//  id("org.metaborg.gradle.config.junit-testing")
 }
 
-val spoofax2Version: String by ext
-val spoofax2DevenvVersion: String by ext
+repositories {
+    mavenCentral()
+    maven("https://nexus.usethesource.io/content/repositories/releases/")
+    maven("https://artifacts.metaborg.org/content/groups/public/")
+}
+
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
-  testImplementation(platform("org.metaborg:parent:$spoofax2Version"))
-  annotationProcessor(platform("org.metaborg:parent:$spoofax2Version"))
-  testAnnotationProcessor(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform(libs.spoofax3.bom))
+    testImplementation(platform(libs.spoofax3.bom))
+    annotationProcessor(platform(libs.spoofax3.bom))
+    testAnnotationProcessor(platform(libs.spoofax3.bom))
 
-  // !! Update dependencies in pom.xml as well
+    implementation(libs.spoofax2.metaborg.util)
+    api(libs.spoofax2.terms)
+    api(libs.spoofax2.interpreter.core)
+    api(libs.capsule)
 
-  implementation("org.metaborg:org.metaborg.util:$spoofax2Version")
-  api("org.metaborg:org.spoofax.terms:$spoofax2Version")
-  api("org.metaborg:org.spoofax.interpreter.core:$spoofax2Version")
-  api("io.usethesource:capsule")
+    // Annotation processing
+    annotationProcessor(libs.immutables.value)
+    annotationProcessor(libs.immutables.serial)
+    compileOnly(libs.immutables.value)
+    compileOnly(libs.immutables.serial)
+    implementation(libs.jakarta.annotation)
 
-  // Annotation processing
-  annotationProcessor("org.immutables:value")
-  annotationProcessor("org.immutables:serial")
-  compileOnly("org.immutables:value")
-  compileOnly("org.immutables:serial")
-  implementation(libs.jakarta.annotation)
+    // Tests
+    testImplementation(libs.junit)
+    testRuntimeOnly(libs.junit.vintage)
+    testImplementation(libs.logback)
 
-  // Tests
-  testImplementation("junit:junit")
-  testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
-  testImplementation("ch.qos.logback:logback-classic")
-
-  // Test Annotation processing
-  testAnnotationProcessor("org.immutables:value")
-  testAnnotationProcessor("org.immutables:serial")
-  testCompileOnly("org.immutables:value")
-  testCompileOnly("org.immutables:serial")
-
-  // !! Update dependencies in pom.xml as well
+    // Test Annotation processing
+    testAnnotationProcessor(libs.immutables.value)
+    testAnnotationProcessor(libs.immutables.serial)
+    testCompileOnly(libs.immutables.value)
+    testCompileOnly(libs.immutables.serial)
 }
 
-// Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
-val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
-  from("$projectDir/src/test/resources")
-  into("$buildDir/classes/java/test")
-}
-tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
+//// Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
+//val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
+//  from("$projectDir/src/test/resources")
+//  into("$buildDir/classes/java/test")
+//}
+//tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,30 @@
 rootProject.name = "nabl"
 
-pluginManagement {
-  repositories {
-    maven("https://artifacts.metaborg.org/content/groups/public/")
-  }
+//pluginManagement {
+//  repositories {
+//    maven("https://artifacts.metaborg.org/content/groups/public/")
+//  }
+//}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from("dev.spoofax:spoofax3-catalog:0.0.0-SNAPSHOT")
+        }
+    }
 }
 
 include("nabl2.terms")
-include("org.metaborg.meta.nabl2.lang")
-project(":org.metaborg.meta.nabl2.lang").projectDir = file("nabl2.lang")
-include("org.metaborg.meta.nabl2.runtime")
-project(":org.metaborg.meta.nabl2.runtime").projectDir = file("nabl2.runtime")
-include("org.metaborg.meta.nabl2.shared")
-project(":org.metaborg.meta.nabl2.shared").projectDir = file("nabl2.shared")
-
-include("statix.solver")
-include("statix.generator")
-include("statix.lang")
-include("statix.runtime")
-
-include("scopegraph")
-include("p_raffrayi")
-
-include("renaming.java")
+//include("nabl2.lang")
+//include("nabl2.runtime")
+//include("nabl2.shared")
+//
+//include("statix.solver")
+//include("statix.generator")
+//include("statix.lang")
+//include("statix.runtime")
+//
+//include("scopegraph")
+//include("p_raffrayi")
+//
+//include("renaming.java")
