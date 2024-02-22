f2eb9754dea4190ebe1acb7d700041c8b8dfe8e1
Jeff Smits <j.smits-1@tudelft.nl>
2024-02-22 17:20:32 +0100

    Fix package named of jsglr.shared

--- nabl2.terms/src/main/java/mb/nabl2/terms/stratego/ATermOrigin.java
+++ nabl2.terms/src/main/java/mb/nabl2/terms/stratego/ATermOrigin.java
@@ -7,8 +7,8 @@ import org.immutables.value.Value;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.terms.attachments.OriginAttachment;
 
-import jsglr.shared.IToken;
-import jsglr.shared.ImploderAttachment;
+import mb.jsglr.shared.IToken;
+import mb.jsglr.shared.ImploderAttachment;
 import mb.nabl2.terms.IAttachments;
 import mb.nabl2.terms.ITerm;
 
