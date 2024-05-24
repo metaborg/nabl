839cf220cdcaffcb8c1eb5211f27871d59515235
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-24 16:36:49 +0200

    Update to Gitonium 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   `java-library`
   id("org.metaborg.gradle.config.root-project") version "0.5.6"
-  id("org.metaborg.gitonium") version "1.1.0"
+  id("org.metaborg.gitonium") version "1.2.0"
   id("org.metaborg.devenv.spoofax.gradle.langspec") version "0.1.34" apply false
 }
 
