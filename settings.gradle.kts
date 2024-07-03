16c2190e3f1a27bfe3478a03e603af7c869fc719
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-03 16:17:10 +0200

    Update version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("org.metaborg.spoofax3:catalog:0.2.2")
+            from("org.metaborg.spoofax3:catalog:0.3.3")
         }
     }
 }
