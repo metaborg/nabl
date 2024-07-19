d7f5ff5323a181eaa03c1f71ed4af6a33fc7bc34
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-19 18:25:20 +0200

    Remove unnecessary maven-publish plugin

--- nabl2.solver/build.gradle.kts
+++ nabl2.solver/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     `java-library`
-    `maven-publish`
     id("org.metaborg.convention.java")
-    id("org.metaborg.convention.junit")
     id("org.metaborg.convention.maven-publish")
+    id("org.metaborg.convention.junit")
 }
 
 dependencies {
